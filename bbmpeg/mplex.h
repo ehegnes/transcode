8d0a1564419781db842ba82740d683fdcb8887e1
Tilmann Bitterberg <transcode@tibit.org>
2003-05-28 11:49:00 +0000

    include <sys/types.h>

--- bbmpeg/mplex.h
+++ bbmpeg/mplex.h
@@ -50,6 +50,8 @@
     Definitionen
 *************************************************************************/
 
+#include <sys/types.h>
+
 #define USER_DATA_START         0x000001b2
 #define SEQUENCE_HEADER 	0x000001b3
 #define SEQUENCE_END		0x000001b7
