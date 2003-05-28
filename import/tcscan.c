4324b0f72b68739128a0a33eb6060822a29af82d
Tilmann Bitterberg <transcode@tibit.org>
2003-05-28 15:49:01 +0000

    add scanning for mp3 files

--- import/tcscan.c
+++ import/tcscan.c
@@ -22,6 +22,8 @@
  */
 
 int enc_bitrate(long frames, double fps, int abit, char *s, int cdsize);
+int tc_get_mp3_header(unsigned char* hbuf, int* chans, int* srate, int *bitrate);
+#define tc_decode_mp3_header(hbuf)  tc_get_mp3_header(hbuf, NULL, NULL)
 
 #undef TCF_DEBUG
 
@@ -388,6 +390,53 @@ int main(int argc, char *argv[])
       
       return(0);
   }
+
+  if(strcmp(codec,"mp3")==0 || magic == TC_MAGIC_MP3) {
+    
+      char header[4];
+      int framesize = 0;
+      int chunks = 0;
+      int srate=0 , chans=0, bitrate=0;
+      unsigned long bitrate_add = 0;
+
+      min = 500;
+      max = 0;
+      
+      // find mp3 header
+      while ((total += read(ipipe.fd_in, header, 4))) {
+	  if ( (framesize = tc_get_mp3_header (header, &chans, &srate, &bitrate)) > 0) {
+	      ++chunks;
+	      bitrate_add += bitrate;
+	      check(bitrate);
+	      break;
+	  }
+      }
+
+      while (on) {
+	  if ( (bytes_read = read(ipipe.fd_in, buffer, framesize-4)) != framesize-4) on = 0;
+	  total += bytes_read;
+	  ++chunks;
+	  while ((total += read(ipipe.fd_in, header, 4))) {
+	      if ( (framesize = tc_get_mp3_header (header, &chans, &srate, &bitrate)) < 0) {
+		  fprintf(stderr, "[%s] corrupt mp3 file?\n", EXE);
+	      } else  {
+		  bitrate_add += bitrate;
+		  check(bitrate);
+		  break;
+	      }
+	  }
+	  //printf("Found new header (%d) (framesize = %d) chan(%d) srate(%d) bitrate(%d)\n", 
+	  //	  chunks, framesize, chans, srate, bitrate);
+      }
+      printf("[%s] found %d MP3 chunks. Average bitrate is %3.2f kbps ", 
+	      EXE, chunks, (double)bitrate_add/chunks);
+      if (min != max) printf("(%d-%d)\n", min, max);
+      else printf("(cbr)\n");
+
+      printf("[%s] AVI overhead will be max. %d*(8+16) = %d bytes (%dk)\n", 
+	      EXE, chunks, chunks*8+chunks*16, (chunks*8+chunks*16)/1024 );
+      return(0);
+  }
   
   /* ------------------------------------------------------------ 
    *
@@ -425,4 +474,104 @@ int main(int argc, char *argv[])
 }
   
   
+// from mencoder
+//----------------------- mp3 audio frame header parser -----------------------
+
+static int tabsel_123[2][3][16] = {
+   { {0,32,64,96,128,160,192,224,256,288,320,352,384,416,448,0},
+     {0,32,48,56, 64, 80, 96,112,128,160,192,224,256,320,384,0},
+     {0,32,40,48, 56, 64, 80, 96,112,128,160,192,224,256,320,0} },
+
+   { {0,32,48,56,64,80,96,112,128,144,160,176,192,224,256,0},
+     {0,8,16,24,32,40,48,56,64,80,96,112,128,144,160,0},
+     {0,8,16,24,32,40,48,56,64,80,96,112,128,144,160,0} }
+};
+static long freqs[9] = { 44100, 48000, 32000, 22050, 24000, 16000 , 11025 , 12000 , 8000 };
+
+/*
+ * return frame size or -1 (bad frame)
+ */
+int tc_get_mp3_header(unsigned char* hbuf, int* chans, int* srate, int *bitrate){
+    int stereo, ssize, crc, lsf, mpeg25, framesize;
+    int padding, bitrate_index, sampling_frequency;
+    unsigned long newhead = 
+      hbuf[0] << 24 |
+      hbuf[1] << 16 |
+      hbuf[2] <<  8 |
+      hbuf[3];
+
+
+#if 1
+    // head_check:
+    if( (newhead & 0xffe00000) != 0xffe00000 ||  
+        (newhead & 0x0000fc00) == 0x0000fc00){
+	//fprintf( stderr, "[%s] head_check failed\n", __FILE__);
+	return -1;
+    }
+#endif
+
+    if((4-((newhead>>17)&3))!=3){ 
+      fprintf( stderr, "[%s] not layer-3\n", __FILE__); 
+      return -1;
+    }
+
+    if( newhead & ((long)1<<20) ) {
+      lsf = (newhead & ((long)1<<19)) ? 0x0 : 0x1;
+      mpeg25 = 0;
+    } else {
+      lsf = 1;
+      mpeg25 = 1;
+    }
+
+    if(mpeg25)
+      sampling_frequency = 6 + ((newhead>>10)&0x3);
+    else
+      sampling_frequency = ((newhead>>10)&0x3) + (lsf*3);
+
+    if(sampling_frequency>8){
+	fprintf( stderr, "[%s] invalid sampling_frequency\n", __FILE__);
+	return -1;  // valid: 0..8
+    }
+
+    crc = ((newhead>>16)&0x1)^0x1;
+    bitrate_index = ((newhead>>12)&0xf);
+    padding   = ((newhead>>9)&0x1);
+//    fr->extension = ((newhead>>8)&0x1);
+//    fr->mode      = ((newhead>>6)&0x3);
+//    fr->mode_ext  = ((newhead>>4)&0x3);
+//    fr->copyright = ((newhead>>3)&0x1);
+//    fr->original  = ((newhead>>2)&0x1);
+//    fr->emphasis  = newhead & 0x3;
+
+    stereo    = ( (((newhead>>6)&0x3)) == 3) ? 1 : 2;
+
+    if(!bitrate_index){
+      fprintf( stderr, "[%s] Free format not supported.\n", __FILE__);
+      return -1;
+    }
+
+    if(lsf)
+      ssize = (stereo == 1) ? 9 : 17;
+    else
+      ssize = (stereo == 1) ? 17 : 32;
+    if(crc) ssize += 2;
+
+    framesize = tabsel_123[lsf][2][bitrate_index] * 144000;
+    if (bitrate) *bitrate = tabsel_123[lsf][2][bitrate_index];
+
+    if(!framesize){
+	fprintf( stderr, "[%s] invalid framesize/bitrate_index\n", __FILE__);
+	return -1;  // valid: 1..14
+    }
+
+    framesize /= freqs[sampling_frequency]<<lsf;
+    framesize += padding;
+
+//    if(framesize<=0 || framesize>MAXFRAMESIZE) return FALSE;
+    if(srate) *srate = freqs[sampling_frequency];
+    if(chans) *chans = stereo;
+
+    return framesize;
+}
+
   
