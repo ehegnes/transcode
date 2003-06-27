8f8f2bd4b241e9c6287d1dbde8085d6221bd5af3
Tilmann Bitterberg <transcode@tibit.org>
2003-06-27 14:23:16 +0000

    small bugfix for yuv mode

--- filter/filter_text.c
+++ filter/filter_text.c
@@ -22,7 +22,7 @@
  */
 
 #define MOD_NAME    "filter_text.so"
-#define MOD_VERSION "v0.1.1 (2003-06-03)"
+#define MOD_VERSION "v0.1.2 (2003-06-27)"
 #define MOD_CAP     "write text in the image"
 #define MOD_AUTHOR  "Tilmann Bitterberg"
 
@@ -58,6 +58,26 @@ enum POS { NONE, TOP_LEFT, TOP_RIGHT, BOT_LEFT, BOT_RIGHT, CENTER, BOT_CENTER };
 
 #define MAX_OPACITY 100
 
+static unsigned char yuv255to224[] = {
+ 16,  17,  18,  19,  20,  20,  21,  22,  23,  24,  25,  26,  27,  27,  28, 
+ 29,  30,  31,  32,  33,  34,  34,  35,  36,  37,  38,  39,  40,  41,  41, 
+ 42,  43,  44,  45,  46,  47,  48,  49,  49,  50,  51,  52,  53,  54,  55, 
+ 56,  56,  57,  58,  59,  60,  61,  62,  63,  63,  64,  65,  66,  67,  68, 
+ 69,  70,  70,  71,  72,  73,  74,  75,  76,  77,  77,  78,  79,  80,  81, 
+ 82,  83,  84,  85,  85,  86,  87,  88,  89,  90,  91,  92,  92,  93,  94, 
+ 95,  96,  97,  98,  99,  99, 100, 101, 102, 103, 104, 105, 106, 106, 107, 
+108, 109, 110, 111, 112, 113, 114, 114, 115, 116, 117, 118, 119, 120, 121, 
+121, 122, 123, 124, 125, 126, 127, 128, 128, 129, 130, 131, 132, 133, 134, 
+135, 135, 136, 137, 138, 139, 140, 141, 142, 142, 143, 144, 145, 146, 147, 
+148, 149, 150, 150, 151, 152, 153, 154, 155, 156, 157, 157, 158, 159, 160, 
+161, 162, 163, 164, 164, 165, 166, 167, 168, 169, 170, 171, 171, 172, 173, 
+174, 175, 176, 177, 178, 179, 179, 180, 181, 182, 183, 184, 185, 186, 186, 
+187, 188, 189, 190, 191, 192, 193, 193, 194, 195, 196, 197, 198, 199, 200, 
+200, 201, 202, 203, 204, 205, 206, 207, 207, 208, 209, 210, 211, 212, 213, 
+214, 215, 215, 216, 217, 218, 219, 220, 221, 222, 222, 223, 224, 225, 226, 
+227, 228, 229, 229, 230, 231, 232, 233, 234, 235, 236, 236, 237, 238, 239, 240
+};
+
 typedef struct MyFilterData {
     /* public */
 	unsigned int start;  /* start frame */
@@ -72,6 +92,7 @@ typedef struct MyFilterData {
 	char *string;        /* text to display */
 	int fade;            /* fade in/out (speed) */
 	int transparent;     /* do not draw a black bounding box */
+	int antialias;       /* do sub frame anti-aliasing (not done) */
 
     /* private */
 	int opaque;          /* Opaqueness of the text */
@@ -120,7 +141,7 @@ int tc_filter(vframe_list_t *ptr, char *options)
   static vob_t *vob=NULL;
 
   static int width=0, height=0;
-  static int size, codec;
+  static int size, codec=0;
   int w, h, i;
   int error;
   time_t mytime = time(NULL);
@@ -155,6 +176,10 @@ int tc_filter(vframe_list_t *ptr, char *options)
 
       snprintf(b, 128, "%d", mfd->fade);
       optstr_param (options, "fade", "fade in/out (0=off, 1=slow, 10=fast)", 
+	      "%d", b, "0", "10");
+
+      snprintf(b, 128, "%d", mfd->antialias);
+      optstr_param (options, "antialias", "Anti-Alias text (0=off 1=on)", 
 	      "%d", b, "0", "10");
 
       optstr_param (options, "pos", "Position (0-width x 0-height)",  
@@ -197,6 +222,7 @@ int tc_filter(vframe_list_t *ptr, char *options)
     mfd->posy=0;
     mfd->pos=NONE;
     mfd->transparent=1;
+    mfd->antialias=1;
 
     mfd->do_time=1;
     mfd->opaque=MAX_OPACITY;
@@ -230,6 +256,7 @@ int tc_filter(vframe_list_t *ptr, char *options)
 	optstr_get (options, "pos",    "%dx%d",    &mfd->posx,  &mfd->posy);
 	optstr_get (options, "string", "%[^:]",    &string);
 	optstr_get (options, "fade",   "%d",       &mfd->fade);
+	optstr_get (options, "antialias",   "%d",       &mfd->antialias);
 
 	if (optstr_lookup (options, "notransparent") )
 	    mfd->transparent = !mfd->transparent;
@@ -276,7 +303,13 @@ int tc_filter(vframe_list_t *ptr, char *options)
       size = width*3/2;
 
     if((buf = (char *)malloc (height*size)) == NULL) return (-1);
-    memset (buf, 0, height*size);
+
+    if (codec == CODEC_RGB)
+	memset (buf, 0, height*size);
+    else {
+	memset (buf, 16, height*width);
+	memset (buf+height*width, 128, height*width/2);
+    }
 
     // init lib
     error = FT_Init_FreeType (&mfd->library);
@@ -383,11 +416,10 @@ int tc_filter(vframe_list_t *ptr, char *options)
 
 	    for (h=0; h<mfd->slot->bitmap.rows; h++) {
 		for (w=0; w<mfd->slot->bitmap.width; w++)  {
-		    unsigned char c = mfd->slot->bitmap.buffer[h*mfd->slot->bitmap.width+w];
-		    c = c>254?254:c;
-		    c = c<20?20:c;
+		    unsigned char c = mfd->slot->bitmap.buffer[h*mfd->slot->bitmap.width+w] &0xff;
+		    c = yuv255to224[c];
 		    // make it transparent
-		    if (mfd->transparent && c==20) continue;
+		    if (mfd->transparent && c==16) continue;
 
 		    p[width*(h+mfd->top_space - mfd->slot->bitmap_top) + 
 			w+mfd->slot->bitmap_left] = c&0xff;
@@ -493,9 +525,12 @@ int tc_filter(vframe_list_t *ptr, char *options)
 
 
 	if (codec == CODEC_YUV) {
+	    char *U, *V;
 
 	    p = ptr->video_buf + mfd->posy*width + mfd->posx;
 	    q =            buf + mfd->posy*width + mfd->posx;
+	    U = ptr->video_buf + mfd->posy/2*width/2 + mfd->posx/2 +  ptr->v_width*ptr->v_height;
+	    V = U + ptr->v_width*ptr->v_height/4;
 
 	    for (h=0; h<mfd->boundY; h++) {
 		for (w=0; w<mfd->boundX; w++)  {
@@ -505,16 +540,18 @@ int tc_filter(vframe_list_t *ptr, char *options)
 		    unsigned int e = 0;
 		    
 		    // transparency
-		    if (mfd->transparent && (c < 20)) continue;
+		    if (mfd->transparent && (c < 16)) continue;
 
 		    // opacity
 		    e = ((MAX_OPACITY-mfd->opaque)*d + mfd->opaque*c)/MAX_OPACITY;
 
 		    // write to image
 		    p[h*width+w] = e&0xff;
+
+		    U[h/2*width/2+w/2] = 128;
+		    V[h/2*width/2+w/2] = 128;
 		}
 	    }
-
 
 
 	} else if (codec == CODEC_RGB) { // FIXME
