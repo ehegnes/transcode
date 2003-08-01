c1a7f1840970f9db44203b7ce72471360d20c424
Tilmann Bitterberg <transcode@tibit.org>
2003-08-01 17:04:10 +0000

    update and bugs fixed

--- filter/filter_modfps.c
+++ filter/filter_modfps.c
@@ -24,13 +24,33 @@
  */
 
 // ----------------- Changes 
+// 0.4 -> 0.5: marrq
+//		initialize memory at runtime.
+//		skip at PRE_S_ clone at POST_S_
+//		fix counting/buffering bugs related to mode=1
 // 0.3 -> 0.4: Tilmann Bitterberg
 //             Fix a typo in the optstr_param printout and correct the filter
 //             flags.
 //             Fix a bug related to scanrange.
 
+// ----------------- TODO
+//	BUG
+//	make this work with at least -c and hopefully filters cut and skip as well.
+//	Currently, when using -c our counters are at 0, which means if we're
+//	dropping frames, we choose to play catch up instead of dropping
+//	and similar if we clone
+//
+//	feature:
+//	Be able to do something fance when cloning.  I'm thinking of four things,
+// 	1. interpolate field of frame which was cloned with frame that
+//	   follows it.
+//	2. evenly merge/average frames.
+//	3. weighted average, so more intence luminance is stronger seen (think
+//	   of phosphors on a TV
+//	4. temporal weighted average
+
 #define MOD_NAME    "filter_modfps.so"
-#define MOD_VERSION "v0.4 (2003-07-31)"
+#define MOD_VERSION "v0.5 (2003-08-01)"
 #define MOD_CAP     "plugin to modify framerate"
 #define MOD_AUTHOR  "Marrq"
 //#define DEBUG 1
@@ -71,6 +91,13 @@ static int infrc  = 0;
 // default settings for NTSC 29.97 -> 23.976
 static int numSample=5;
 static int offset = 32;
+static int runnow = 0;
+
+static char **frames = NULL;
+static int frbufsize;
+static int frameIn = 0, frameOut = 0;
+static int *framesOK, *framesScore;
+static int scanrange = 0;
 
 static double frc_table[16] = {0,
 			       NTSC_FILM, 24, 25, NTSC_VIDEO, 30, 50, 
@@ -88,24 +115,71 @@ static void help_optstr()
     printf ("  There are two modes of operation, buffered and unbuffered,\n");
     printf ("  unbuffered is quick, but buffered, especially when dropping frames\n");
     printf ("  should look better\n");
+    printf ("  For most users, modfps will need either no options, or just mode=1\n");
     printf ("* Options\n");
     printf ("\tmode : (0=unbuffered, 1=buffered [%d]\n", mode);
-    printf ("\tinfps : original fps (needed) [%lf]\n",infps);
+    printf ("\tinfps : original fps (override what transcode supplies) [%lf]\n",infps);
     printf ("\tinfrc : original frc (overwrite infps) [%d]\n",infrc);
     printf ("\tbuffer : number of frames to buffer [%d]\n",numSample);
     printf ("\tsubsample : number of pixels to subsample when examining buffers [%d]\n",offset);
     printf ("\tverbose : 0 = not verbose, 1 is verbose [%d]\n",show_results);
 }
 
+static int memory_init(vframe_list_t * ptr){
+
+  int i;
+  frbufsize = numSample +1;
+  if (ptr->v_codec == CODEC_YUV){
+    // we only care about luminance
+    scanrange = ptr->v_height*ptr->v_width;
+  } else if (ptr->v_codec == CODEC_RGB){
+    scanrange = ptr->v_height*ptr->v_width*3;
+  } else if (ptr->v_codec == CODEC_YUY2){
+    // we only care about luminance, but since this is packed
+    // we'll look at everything.
+    scanrange = ptr->v_height*ptr->v_width*2;
+  }
+
+  if (scanrange > ptr->video_size){
+    // error, we'll overwalk boundaries later on
+    fprintf(stderr, "[%s] Error, video_size doesn't look to be big enough.\n");
+    return -1;
+  }
+  
+  frames = (char**)malloc(sizeof (char*)*frbufsize);
+  if (NULL == frames){
+    fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
+    return -1;
+  } // else
+  for (i=0;i<frbufsize; i++){
+    frames[i] = (char*)malloc(sizeof(char)*ptr->video_size);
+    if (NULL == frames[i]){
+      fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
+      return -1;
+    }
+  }
+  framesOK = (int*)malloc(sizeof(int)*frbufsize);
+  if (NULL == framesOK){
+    fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
+    return -1;
+  }
+  framesScore = (int*)malloc(sizeof(int)*frbufsize);
+  if (NULL == framesScore){
+    fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
+    return -1;
+  }
+  if (mode == 1){
+    return 0;
+  }
+  return -1;
+}
+
 int tc_filter(vframe_list_t * ptr, char *options)
 {
     static vob_t *vob = NULL;
     static int frameCount = 0;
-    static char **frames = NULL;
-    static int frbufsize;
-    static int frameIn = 0, frameOut = 0;
-    static int *framesOK, *framesScore;
-    static int scanrange = 0;
+    static int init = 1;
+    static int cloneq = 0; // queue'd clones ;)
 
     static double outfps = 0.0;
 
@@ -125,8 +199,6 @@ int tc_filter(vframe_list_t * ptr, char *options)
 	outfps = vob->ex_fps;
 	infps  = vob->fps;
 	infrc  = vob->im_frc;
-	if (infrc>0 && infrc < 16)
-	    infps = frc_table[infrc];
 
 	// filter init ok.
 	if (options != NULL) {
@@ -142,57 +214,37 @@ int tc_filter(vframe_list_t * ptr, char *options)
 
 	}
 
-	if (verbose)
-	    printf("[%s] %s %s\n", MOD_NAME, MOD_VERSION, MOD_CAP);
+	if (infrc>0 && infrc < 16){
+	  infps = frc_table[infrc];
+	}
 
-	  if (outfps > infps*2.0){
-	    fprintf(stderr, "[%s] Error, desired output fps can not be greater\n",MOD_NAME);
-	    fprintf(stderr, "[%s] than twice the input fps\n", MOD_NAME);
-	    return -1;
-	  }
+	if (verbose){
+	  printf("[%s] %s %s\n", MOD_NAME, MOD_VERSION, MOD_CAP);
+	  printf("[%s] converting from %2.4ffps to %2.4ffps\n",MOD_NAME,infps,outfps);
+	}
 
-	  if ( (outfps == infps) || (infrc && infrc == vob->ex_frc)) {
-	    fprintf(stderr, "[%s] No framerate conversion requested, exiting\n",MOD_NAME);
-	    return -1;
-	  }
+	if (outfps > infps*2.0){
+	  fprintf(stderr, "[%s] Error, desired output fps can not be greater\n",MOD_NAME);
+	  fprintf(stderr, "[%s] than twice the input fps\n", MOD_NAME);
+	  return -1;
+	}
 
+	if ( (outfps == infps) || (infrc && infrc == vob->ex_frc)) {
+	  fprintf(stderr, "[%s] No framerate conversion requested, exiting\n",MOD_NAME);
+	  return -1;
+	}
 
-	if (mode == 0){
+	// clone in POST_S skip in PRE_S
+	if (outfps > infps){
+	  runnow = TC_POST_S_PROCESS;
+	} else {
+	  runnow = TC_PRE_S_PROCESS;
+	}
+
+	if ((mode >= 0) && (mode < 2)){
 	  return 0;
 	} // else
 
-	// can't do that here, its not valid. ptr->video_size will contain the
-	// maximum of the frame the filter may get. 
-	//scanrange = ptr->video_size;
-	{ // allocate buffers
-	  int i;
-	  frbufsize = numSample +1;
-	  frames = (char**)malloc(sizeof (char*)*frbufsize);
-	  if (NULL == frames){
-	    fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
-	    return -1;
-	  } // else
-	  for (i=0;i<frbufsize; i++){
-	    frames[i] = (char*)malloc(sizeof(char)*ptr->video_size);
-	    if (NULL == frames[i]){
-	      fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
-	      return -1;
-	    }
-	  }
-	  framesOK = (int*)malloc(sizeof(int)*frbufsize);
-	  if (NULL == framesOK){
-	    fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
-	    return -1;
-	  }
-	  framesScore = (int*)malloc(sizeof(int)*frbufsize);
-	  if (NULL == framesScore){
-	    fprintf(stderr, "[%s] Error allocating memory in init\n",MOD_NAME);
-	    return -1;
-	  }
-	  if (mode == 1){
-	    return 0;
-	  }
-	}
 	fprintf (stderr, "[%s] Error, only two modes of operation.\n",MOD_NAME);
 	return -1;
     }
@@ -238,10 +290,10 @@ int tc_filter(vframe_list_t * ptr, char *options)
     // transcodes internal video/audio frame processing routines
     // or after and determines video/audio context
 
-    if ((ptr->tag & TC_PRE_S_PROCESS) && (ptr->tag & TC_VIDEO)) {
+    if ((ptr->tag & runnow) && (ptr->tag & TC_VIDEO)) {
       if (mode == 0){
         if (show_results){
-          printf("[%s] infps=%02.4lf outfps=%02.4lf mycount=%5d id=%5d calc=%06.3lf\t",MOD_NAME,infps,outfps,frameCount,ptr->id,(double)frameCount*infps/outfps);
+          printf("[%s] mycount=%5d id=%5d calc=%06.3lf ",MOD_NAME,frameCount,ptr->id,(double)frameCount*infps/outfps);
 	} 
         if (infps < outfps){
 	  // Notes; since we currently only can clone frames (and just clone
@@ -254,7 +306,6 @@ int tc_filter(vframe_list_t * ptr, char *options)
 	      printf("Ack, this frame was cloned!\n");
 	    }
 	    ptr->attributes |= TC_FRAME_IS_CLONED;
-	    ++frameCount;
 	  }
 	} else {
 	  if ((double)frameCount*infps/outfps > (double)ptr->id){
@@ -273,19 +324,41 @@ int tc_filter(vframe_list_t * ptr, char *options)
       } // else
       if (mode == 1){
         int i;
-	if (show_results){
-          printf("[%s] infps=%02.4lf outfps=%02.4lf mycount=%5d id=%5d calc=%06.3lf ",MOD_NAME,infps,outfps,frameCount-numSample,ptr->id-numSample,(double)(frameCount-numSample)*infps/outfps);
+	if (init){
+	  init = 0;
+	  i = memory_init(ptr);
+	  if(i!=0){
+	    return i;
+	  }
 	}
-	//printf("frames=%p frameIn=%p next=%p ptr=%p size=%d\n",
-	//  frames,frames[frameIn],frames[(frameIn+1)%frbufsize],ptr->video_buf,ptr->video_size);
-        memcpy(frames[frameIn], ptr->video_buf, ptr->video_size);
+	if (show_results){
+          printf("[%s] frameIn=%d frameOut=%d mycount=%5d id=%5d calc=%06.3lf ",MOD_NAME,frameIn,frameOut,frameCount-numSample,ptr->id-numSample-cloneq,(double)(frameCount-numSample)*infps/outfps);
+	}
+	if (ptr->attributes & TC_FRAME_WAS_CLONED){
+	  // don't do anything.  Since it's cloned, we don't
+	  // want to put it our buffers, as it will just clog
+	  // them up.  Later, we can try some merging/interpolation
+	  // as the user requests and then leave, but for now, we'll
+	  // just flee.
+	  if (framesOK[(frameIn+0)%frbufsize]){
+	    fprintf(stderr, "[%s] Oppps, this frame wasn't cloned but we thought it was\n",MOD_NAME);
+	  }
+	  ++frameCount;
+	  --cloneq;
+	  if (show_results){
+	    printf("no slot needed for clones\n");
+	  }
+	  return 0;
+	} // else 
+	memcpy(frames[frameIn], ptr->video_buf, ptr->video_size);
+	framesOK[frameIn] = 1;
 #ifdef DEBUG
-	  fprintf(stderr, "Inserted frame %d into slot %d \n",frameCount, frameIn);
+	printf("Inserted frame %d into slot %d \n",frameCount, frameIn);
 #endif // DEBUG
-	framesOK[frameIn] = 1; 
-	fflush(stdout);
+
+	// Now let's look and see if we should compute a frame's
+	// score.
 	if (frameCount > 0){
-	  // we have a frame before this one, so we'll compute its score
 	  char *t1, *t2;
 	  int *score,t;
 	  t=(frameIn+numSample)%frbufsize;
@@ -293,8 +366,8 @@ int tc_filter(vframe_list_t * ptr, char *options)
 	  t1 = frames[t];
 	  t2 = frames[frameIn];
 #ifdef DEBUG
-	    printf("score: offset=%d, offset-1=%d, score=%p t1=%p t2=%p ",
-	      frameIn,t,score,t1,t2);
+	    printf("score: slot=%d, t1=%p t2=%p ",
+	      t,t1,t2);
 #endif // DEBUG
 	  *score=0;
 	  for(i=0; i<ptr->video_size; i+=offset){
@@ -317,11 +390,12 @@ int tc_filter(vframe_list_t * ptr, char *options)
 	  }
 	  return 0;
 	} // else
+
 	// having filled the buffer, we will now check to see if we
-	// are ready to skip a frame.  If we are, we look for the frame to skip
+	// are ready to clone/skip a frame.  If we are, we look for the frame to skip
 	// in the buffer
 	if (infps < outfps){
-	  if ((double)(frameCount-numSample)*infps/outfps < (double)(ptr->id-numSample)){
+	  if ((double)(frameCount-numSample)*infps/outfps < (double)(ptr->id-numSample-cloneq)){
 	    // we have to find a frame to clone
 	    int diff=-1, mod=-1;
 #ifdef DEBUG
@@ -347,9 +421,9 @@ int tc_filter(vframe_list_t * ptr, char *options)
 	      return -1;
 	    }
 #ifdef DEBUG
-	      printf("XXX cloning %d\n",mod);
+	    printf("XXX cloning  %d\n",mod);
 #endif // DEBUG
-	    ++frameCount;
+	    ++cloneq;
 	    framesOK[mod] = 0;
 	  }
 	  memcpy(ptr->video_buf,frames[frameOut],ptr->video_size);
@@ -368,6 +442,9 @@ int tc_filter(vframe_list_t * ptr, char *options)
 	  // check to skip frames
 	  if ((double)(frameCount-numSample)*infps/outfps > (double)(ptr->id-numSample)){
 	    int diff=INT_MAX, mod=-1;
+
+	    // since we're skipping, we look for the frame with the lowest
+	    // difference between the frame which follows it.
 	    for(i=((frameIn+1)%frbufsize); i!=frameIn; i=((i+1)%frbufsize)){
 #ifdef DEBUG
 	        printf("i=%d Ok=%d Score=%d\n",i,framesOK[i],framesScore[i]);
