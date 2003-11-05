65654a9e3272f5b6fdbc587f086d9bd889bdd3e1
Tilmann Bitterberg <transcode@tibit.org>
2003-11-05 17:24:48 +0000

    avilib fixes from Moritz Bunkus

---
+++ avilib/os.h
@@ -0,0 +1,29 @@
+#ifndef __OS_H
+#define __OS_H
+
+#if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__)
+# define SYS_WINDOWS
+# if defined __MINGW32__
+#  define COMP_MINGW
+# elif defined __CYGWIN__
+#  define COMP_CYGWIN
+# else
+#  define COMP_MSC
+# endif
+#elif defined(__APPLE__)
+# define SYS_APPLE
+#else
+# define COMP_GCC
+# define SYS_UNIX
+# if defined(__bsdi__) || defined(__FreeBSD__) 
+#  define SYS_BSD
+# else
+#  define SYS_LINUX
+# endif
+#endif
+
+#if !defined(COMP_CYGWIN)
+#include <stdint.h>
+#endif // !COMP_CYGWIN
+
+#endif
