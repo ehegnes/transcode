03dd80d8ac94034615f189a77ceaeff519b2401b
Tilmann Bitterberg <transcode@tibit.org>
2003-11-05 17:52:20 +0000

    fixup

--- avilib/avilib.c
+++ avilib/avilib.c
@@ -2443,7 +2443,7 @@ long AVI_audio_size(avi_t *AVI, long frame)
   if(AVI->mode==AVI_MODE_WRITE) { AVI_errno = AVI_ERR_NOT_PERM; return -1; }
   if(!AVI->track[AVI->aptr].audio_index)         { AVI_errno = AVI_ERR_NO_IDX;   return -1; }
   
-  if(frame < 0 || frame >= AVI->track[AVI->aptr].audio_chunks) return 0;
+  if(frame < 0 || frame >= AVI->track[AVI->aptr].audio_chunks) return -1;
   return(AVI->track[AVI->aptr].audio_index[frame].len);
 }
 
