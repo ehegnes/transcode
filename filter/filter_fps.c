0523bf10ec96809c0d2631b478cd20e657feb732
Tilmann Bitterberg <transcode@tibit.org>
2004-02-24 16:20:06 +0000

    Add missing string termination in parse_options().   Add pre and post options to parse_options().

--- filter/filter_fps.c
+++ filter/filter_fps.c
@@ -1,7 +1,7 @@
 /*
  *  filter_fps.c
  *
- *  Copyright 2003 Christopher Cramer
+ *  Copyright 2003, 2004 Christopher Cramer
  *
  *  This file is part of transcode, a linux video stream processing tool
  *      
@@ -22,7 +22,7 @@
  */
 
 #define MOD_NAME    "filter_fps.so"
-#define MOD_VERSION "v0.2 (2003-08-10)"
+#define MOD_VERSION "v1.0 (2004-02-19)"
 #define MOD_CAP     "convert video frame rate, gets defaults from -f and --export_fps"
 
 #include <stdio.h>
@@ -40,17 +40,19 @@
 #include "optstr.h"
 
 static int
-parse_options(char *options, double *infps, double *outfps)
+parse_options(char *options, int *pre, double *infps, double *outfps)
 {
 	char	*p, *q, *r;
 	size_t	len;
 	vob_t	*vob;
+	int	default_pre;
 
 	/* defaults from -f and --export_fps */
 	vob = tc_get_vob();
 	if (!vob) return -1;
 	*infps = vob->fps;
 	*outfps = vob->ex_fps;
+	default_pre = 1;
 
 	if (!options || !*options) return 0;
 	if (!strcmp(options, "help")) {
@@ -60,8 +62,13 @@ parse_options(char *options, double *infps, double *outfps)
 			"options: <input fps>:<output fps>\n"
 			"example: -J fps=25:29.97 will convert"
 			" from PAL to NTSC\n"
-			"If no options are given, defaults"
-			" or -f/--export_fps/--export_frc will be used.\n",
+			"In addition to the frame rate options,"
+			" you may also specify pre or post."
+			"If no rate options are given, defaults"
+			" or -f/--export_fps/--export_frc will be used.\n"
+			"If no pre or post options are given,"
+			" decreasing rates will preprocess and"
+			" increasing rates will postprocess.",
 			MOD_NAME);
 		return -1;
 	}
@@ -69,13 +76,44 @@ parse_options(char *options, double *infps, double *outfps)
 	len = strlen(options);
 	p = alloca(len + 1);
 	memcpy(p, options, len);
-	q = memchr(p, ':', len);
-	if (!q) return -1;
-	*q++ = '\0';
-	*infps = strtod(p, &r);
-	if (r == p) return -1;
-	*outfps = strtod(q, &r);
-	if (r == q) return -1;
+	p[len] = '\0';
+
+	i = 0;
+	do {
+		q = memchr(p, ':', len);
+		if (q) *q++ = '\0';
+		if (!strcmp(p, "pre")) {
+			*pre = 1;
+			default_pre = 0;
+		} else if (!strncmp(p, "pre=", 4) && *(p + 4)) {
+			*pre = strtol(p + 4, &r, 0);
+			if (r == p) return -1;
+			default_pre = 0;
+		} else if (!strcmp(p, "post")) {
+			*pre = 0;
+			default_pre = 0;
+		} else if (!strncmp(p, "post=", 5) && *(p + 5)) {
+			*pre = !strtol(p + 4, &r, 0);
+			if (r == p) return -1;
+			default_pre = 0;
+		} else {
+			if (i == 0) {
+				*infps = strtod(p, &r);
+				if (r == p) return -1;
+			} else if (i == 1) {
+				*outfps = strtod(p, &r);
+				if (r == p) return -1;
+			} else return -1;
+			i++;
+		}
+	} while (q && (p = q));
+
+	if (default_pre) {
+		if (*infps > *outfps) *pre = 1;
+		else if (*infps < *outfps) *pre = 0;
+	}
+
+	return 0;
 }
 
 int
@@ -83,35 +121,36 @@ tc_filter(vframe_list_t *ptr, char *options)
 {
 	static double		infps, outfps;
 	static unsigned long	framesin = 0, framesout = 0;
+	static int		pre = 0;
 
 	if(ptr->tag & TC_FILTER_GET_CONFIG) {
-	    optstr_filter_desc (options, MOD_NAME, MOD_CAP, MOD_VERSION, "Christopher Cramer", "VRYEO", "1");
+	    optstr_filter_desc (options, MOD_NAME, MOD_CAP, MOD_VERSION,
+		"Christopher Cramer", "VRYEO", "1");
 	    return 0;
 	}
 
 	if(ptr->tag & TC_FILTER_INIT) {
 		if (verbose) printf("[%s] %s %s\n",
 			MOD_NAME, MOD_VERSION, MOD_CAP);
-		if (parse_options(options, &infps, &outfps) == -1) return -1;
-		if (verbose && options) printf(
-			"[%s] options=%s, converting from %g fps to %g fps\n",
-			MOD_NAME, options, infps, outfps);
-		if (verbose && !options) printf(
-			"[%s] no options, converting from %g fps to %g fps\n",
-			MOD_NAME, infps, outfps);
+		if (parse_options(options, &pre, &infps, &outfps) == -1)
+			return -1;
+		if (verbose && options) printf("[%s] options=%s",
+			MOD_NAME, options);
+		if (verbose && !options) printf("[%s] no options");
+		if (verbose) printf(", converting from %g fps to %g fps,"
+			" %sprocessing\n", infps, outfps,
+			pre ? "pre" : "post");
 		return 0;
 	}
 
-	if (infps > outfps && ptr->tag & TC_PRE_S_PROCESS
-			&& ptr->tag & TC_VIDEO) {
+	if (pre && ptr->tag & TC_PRE_S_PROCESS && ptr->tag & TC_VIDEO) {
 		if ((double)++framesin / infps > (double)framesout / outfps)
 			framesout++;
 		else ptr->attributes |= TC_FRAME_IS_SKIPPED;
 		return 0;
 	}
 
-	if (infps < outfps && ptr->tag & TC_POST_S_PROCESS
-			&& ptr->tag & TC_VIDEO) {
+	if (!pre && ptr->tag & TC_POST_S_PROCESS && ptr->tag & TC_VIDEO) {
 		if (!(ptr->attributes & TC_FRAME_WAS_CLONED)) framesin++;
 		if ((double)framesin / infps > (double)++framesout / outfps)
 			ptr->attributes |= TC_FRAME_IS_CLONED;
