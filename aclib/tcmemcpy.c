c8430d2ec552bee7c79cb056049461831a05aa46
Jacob Meuser <jakemsr@sdf.lonestar.org>
2004-10-17 22:38:46 +0000

    2004/08/11 13:05:20
    *  aclib/tcmemcpy.c
    
    Implementation of tc_memcpy (and tc_memcpy_init)

---
+++ aclib/tcmemcpy.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include "ac.h"
+
+void * (*tc_memcpy)(void *, const void *, size_t) = memcpy;
+
+void tc_memcpy_init(int verbose, int mmflags)
+{
+	int accel = mmflags == -1 ? ac_mmflag() : mmflags;
+	const char * method = "libc";
+	
+#ifdef ARCH_X86
+	if((accel && MM_MMXEXT) || (accel & MM_SSE))
+	{
+		method = "amdmmx";
+		tc_memcpy = ac_memcpy_amdmmx;
+	}
+#endif
+
+	if(verbose)
+		fprintf(stderr, "tc_memcpy: using %s for memcpy\n", method);
+}
