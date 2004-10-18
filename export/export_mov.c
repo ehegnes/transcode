22b8d4aa35e2ec044d3cd94a6e63116f169eed94
Jacob Meuser <jakemsr@sdf.lonestar.org>
2004-10-18 00:36:37 +0000

    remove extra comment line

--- export/export_mov.c
+++ export/export_mov.c
@@ -23,7 +23,6 @@
  *  You should have received a copy of the GNU General Public License
  *  along with GNU Make; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
- *
  */
 
 #include <stdio.h>
