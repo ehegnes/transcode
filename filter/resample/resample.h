e67434bfa4539fd0c352ad56bb993e1db5141522
Jacob Meuser <jakemsr@sdf.lonestar.org>
2004-10-24 22:26:57 +0000

    remove files that are now unused

--- filter/resample/resample.h
+++
@@ -1,6 +0,0 @@
-#ifndef RESAMPLE_H
-#define RESAMPLE_H
-int filter_resample_stop(char *stopo);
-int filter_resample_flow(char *flowi, int isamp, char *flowo);
-int filter_resample_init(int irate, int orate);
-#endif
