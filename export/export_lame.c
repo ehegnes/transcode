796fa1733313d316e7dab43ca714f2ee0cde781c
Jacob Meuser <jakemsr@sdf.lonestar.org>
2004-11-04 01:41:14 +0000

    the -x switch is endian dependent, or, well, it would be if it wasn't set explicitly ...

--- export/export_lame.c
+++ export/export_lame.c
@@ -142,7 +142,7 @@ MOD_open
       break;
     }      
     
-    sprintf(ptr, "lame -x %s %s %d.%03d -m %c - \"%s.mp3\" 2>/dev/null %s", 
+    sprintf(ptr, "lame %s %s %d.%03d -m %c - \"%s.mp3\" 2>/dev/null %s", 
 	    swap_bytes, br, ofreq_int, ofreq_dec, chan, vob->audio_out_file, (vob->ex_a_string?vob->ex_a_string:""));
     
     fprintf (stderr,"[%s] cmd=%s\n", MOD_NAME, buf);
