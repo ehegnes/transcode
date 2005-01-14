1dc42a2051094da57df60c9ea960daed90974df2
Jacob Meuser <jakemsr@sdf.lonestar.org>
2005-01-14 05:29:22 +0000

    get ready for AMD64
    taken from ffmpeg

--- aclib/cputest.c
+++ aclib/cputest.c
@@ -11,18 +11,26 @@
 #endif
 #include "ac.h"
 
+#ifdef ARCH_X86_64
+#  define REG_b "rbx"
+#  define REG_S "rsi"
+#else
+#  define REG_b "ebx"
+#  define REG_S "esi"
+#endif
+
 //exported
 int mm_flag=-1;
 
 /* ebx saving is necessary for PIC. gcc seems unable to see it alone */
 #define cpuid(index,eax,ebx,ecx,edx)\
     __asm __volatile\
-	("movl %%ebx, %%esi\n\t"\
+	("mov %%"REG_b", %%"REG_S"\n\t"\
          "cpuid\n\t"\
-         "xchgl %%ebx, %%esi"\
+         "xchg %%"REG_b", %%"REG_S\
          : "=a" (eax), "=S" (ebx),\
            "=c" (ecx), "=d" (edx)\
-         : "a" (index));
+         : "0" (index));
 
 #define CPUID_STD_MMX          0x00800000
 #define CPUID_STD_SSE          0x02000000
@@ -37,105 +45,114 @@ int mm_flag=-1;
 /* Function to test if multimedia instructions are supported...  */
 static int mm_support(void)
 {
-#ifdef ARCH_X86
-    int rval;
+    int rval = 0;
+#if defined(ARCH_X86) || defined(ARCH_X86_64)
     int eax, ebx, ecx, edx;
+    int max_std_level, max_ext_level, std_caps=0, ext_caps=0;
+    long a, c;
+
     char vendor[13] = "UnknownVndr";
     
     __asm__ __volatile__ (
                           /* See if CPUID instruction is supported ... */
                           /* ... Get copies of EFLAGS into eax and ecx */
                           "pushf\n\t"
-#ifdef __x86_64__
                           "pop %0\n\t"
-#else
-                          "popl %0\n\t"
-#endif
-                          "movl %0, %1\n\t"
+                          "mov %0, %1\n\t"
                           
                           /* ... Toggle the ID bit in one copy and store */
                           /*     to the EFLAGS reg */
-                          "xorl $0x200000, %0\n\t"
+                          "xor $0x200000, %0\n\t"
                           "push %0\n\t"
                           "popf\n\t"
                           
                           /* ... Get the (hopefully modified) EFLAGS */
                           "pushf\n\t"
-#ifdef __x86_64__
                           "pop %0\n\t"
-#else
-                          "popl %0\n\t"
-#endif
-                          : "=a" (eax), "=c" (ecx)
+                          : "=a" (a), "=c" (c)
                           :
                           : "cc" 
                           );
     
-    if (eax == ecx)
+    if (a == c)
         return 0; /* CPUID not supported */
+
+    /* highest cpuid is 0, no standard features */
+    if (eax == 0)
+        return rval;
     
-    cpuid(0, eax, ebx, ecx, edx);
+    cpuid(0, max_std_level, ebx, ecx, edx);
 
     /* save the vendor string */
     *(int *)vendor = ebx;
     *(int *)&vendor[4] = edx;
     *(int *)&vendor[8] = ecx;
 
-    rval = 0;
-
-    /* highest cpuid is 0, no standard features */
-    if (eax == 0)
-        return rval;
-
-    /* get standard features */
-    cpuid(1, eax, ebx, ecx, edx); 
-    //printf("CPUID 1 eax=0x%8.8x ebx=0x%8.8x ecx=0x%8.8x edx=0x%8.8x\n", eax, ebx, ecx, edx);
-    if (edx & CPUID_STD_MMX)
-        rval |= MM_MMX;
-    if (edx & CPUID_STD_SSE)
-        rval |= MM_MMXEXT | MM_SSE;
-    if (edx & CPUID_STD_SSE2)
-        rval |= MM_SSE2;
-    if (ecx & CPUID_STD_SSE3)
-        rval |= MM_SSE3;
-
-    /* check for extended feature flags support */
-    cpuid(0x80000000, eax, ebx, ecx, edx);
-    //printf("CPUID 8-0 eax=0x%8.8x ebx=0x%8.8x ecx=0x%8.8x edx=0x%8.8x\n", eax, ebx, ecx, edx);
-    if (eax < 0x80000001)
-        return rval;
-
-    /* get extended feature flags */
-    cpuid(0x80000001, eax, ebx, ecx, edx);
-    //printf("CPUID 8-1 eax=0x%8.8x ebx=0x%8.8x ecx=0x%8.8x edx=0x%8.8x\n", eax, ebx, ecx, edx);
-
-    /* AMD-specific extensions */
-    if (strcmp(vendor, "AuthenticAMD") == 0) {
-        if (edx & CPUID_EXT_AMD_3DNOW)
-            rval |= MM_3DNOW;
-        if (edx & CPUID_EXT_AMD_3DNOWEXT)
-            rval |= MM_3DNOWEXT;
-        /* if no MMXEXT already, check AMD-specific flag */
-        if (((rval & MM_MMXEXT) == 0) && (edx & CPUID_EXT_AMD_MMXEXT))
-            rval |= MM_MMXEXT;
-    }
-
-    /* Cyrix-specific extensions */
-    else if (strcmp(vendor, "CyrixInstead") == 0) {
-        /* The Cyrix CPU Detection Guide says that CPUID level 1 should 
-           return the proper MMX feature flag, but just in case */
-        if (((rval & MM_MMX) == 0) && (edx & CPUID_EXT_CYR_MMX))
+    if(max_std_level >= 1){
+        cpuid(1, eax, ebx, ecx, std_caps);
+        if (std_caps & (1<<23))
             rval |= MM_MMX;
-        /* if no MMXEXT already, check Cyrix-specific flag */
-        if (((rval & MM_MMXEXT) == 0) && (edx & CPUID_EXT_CYR_MMXEXT))
-            rval |= MM_MMXEXT;
+        if (std_caps & (1<<25)) 
+            rval |= MM_MMXEXT | MM_SSE;
+        if (std_caps & (1<<26)) 
+            rval |= MM_SSE2;
     }
 
-    return rval;
-#else // not X86
-    return 0;
+    cpuid(0x80000000, max_ext_level, ebx, ecx, edx);
+
+    if(max_ext_level >= 0x80000001){
+        cpuid(0x80000001, eax, ebx, ecx, ext_caps);
+        if (ext_caps & (1<<31))
+            rval |= MM_3DNOW;
+        if (ext_caps & (1<<30))
+            rval |= MM_3DNOWEXT;
+        if (ext_caps & (1<<23))
+            rval |= MM_MMX;
+    }
+
+    cpuid(0, eax, ebx, ecx, edx);
+    if (       ebx == 0x68747541 &&
+               edx == 0x69746e65 &&
+               ecx == 0x444d4163) {
+        /* AMD */
+        if(ext_caps & (1<<22))
+            rval |= MM_MMXEXT;
+    } else if (ebx == 0x746e6543 &&
+               edx == 0x48727561 &&
+               ecx == 0x736c7561) {  /*  "CentaurHauls" */
+        /* VIA C3 */
+	if(ext_caps & (1<<24))
+	  rval |= MM_MMXEXT;
+    } else if (ebx == 0x69727943 &&
+               edx == 0x736e4978 &&
+               ecx == 0x64616574) {
+        /* Cyrix Section */
+        /* See if extended CPUID level 80000001 is supported */
+        /* The value of CPUID/80000001 for the 6x86MX is undefined
+           according to the Cyrix CPU Detection Guide (Preliminary
+           Rev. 1.01 table 1), so we'll check the value of eax for
+           CPUID/0 to see if standard CPUID level 2 is supported.
+           According to the table, the only CPU which supports level
+           2 is also the only one which supports extended CPUID levels.
+        */
+        if (eax < 2) 
+            return rval;
+        if (ext_caps & (1<<24))
+            rval |= MM_MMXEXT;
+    }
+#if 0
+    av_log(NULL, AV_LOG_DEBUG, "%s%s%s%s%s%s\n", 
+        (rval&MM_MMX) ? "MMX ":"", 
+        (rval&MM_MMXEXT) ? "MMX2 ":"", 
+        (rval&MM_SSE) ? "SSE ":"", 
+        (rval&MM_SSE2) ? "SSE2 ":"", 
+        (rval&MM_3DNOW) ? "3DNow ":"", 
+        (rval&MM_3DNOWEXT) ? "3DNowExt ":"");
 #endif
+#endif  /* ARCH_X86(_64) */
+    return rval;
 }
+
 
 int ac_mmflag(void) 
 {
