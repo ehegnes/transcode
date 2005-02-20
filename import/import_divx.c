920fce65d813eaf6457a16411fc6a622cbc1346b
Jacob Meuser <jakemsr@sdf.lonestar.org>
2005-02-20 10:45:53 +0000

    kill useless space characters. don't define a local macro if a global macro will work. respect 80 character wide screens. add some tc_test_string() calls.

--- import/import_divx.c
+++ import/import_divx.c
@@ -34,7 +34,6 @@ static int capability_flag = TC_CAP_RGB | TC_CAP_YUV | TC_CAP_VID;
 #define MOD_PRE divx
 #include "import_def.h"
 
-
 #ifdef HAVE_DLFCN_H
 #include <dlfcn.h>
 #else
@@ -49,17 +48,16 @@ static int capability_flag = TC_CAP_RGB | TC_CAP_YUV | TC_CAP_VID;
 #include "divx_decore.h"
 #endif
 
+
 #ifndef DECORE_VERSION
 #define DECORE_VERSION 0
 #endif
 
-#define MAX_BUF 1024
-char import_cmd_buf[MAX_BUF];
+char import_cmd_buf[TC_BUF_MAX];
 static int codec, frame_size=0;
 static unsigned long divx_version=DEC_OPT_FRAME;
 
 static int done_seek=0;
-
 
 static DEC_FRAME    *decFrame = NULL;
 static DEC_FRAME_INFO *decInfo = NULL;
@@ -69,12 +67,14 @@ static DEC_FRAME_INFO *decInfo = NULL;
   static void* dec_handle = NULL;
   static DEC_INIT *decInit = NULL;
   DivXBitmapInfoHeader* pbi=NULL;
-  static int (*divx_decore)(void *para0, unsigned long opt, void *para1, void *para2);
+  static int (*divx_decore)(void *para0, unsigned long opt,
+                            void *para1, void *para2);
 
 #else
 
   static DEC_PARAM    *divx = NULL;
-  static int (*divx_decore)(unsigned long para0, unsigned long opt, void *para1, void *para2);
+  static int (*divx_decore)(unsigned long para0, unsigned long opt,
+                            void *para1, void *para2);
   static unsigned long divx_id=0x4711;
 
 #endif
@@ -132,17 +132,17 @@ int divx4_is_key(unsigned char *data, long size)
                                 return 0;
                 }
         }
-        
+
         return result;
 }
 
 int divx3_is_key(char *d)
 {
     int32_t c=0;
-    
+
     c=stream_read_dword(d);
     if(c&0x40000000) return(0);
-    
+
     return(1);
 }
 
@@ -162,7 +162,7 @@ static unsigned char *bufalloc(size_t size)
    if (buf == NULL) {
        fprintf(stderr, "(%s) out of memory", __FILE__);
    }
-   
+
    adjust = buffer_align - ((long) buf) % buffer_align;
 
    if (adjust == buffer_align)
@@ -176,43 +176,49 @@ static int divx_init(char *path) {
     const
 #endif    
     char *error;
+    int sret;
 
     handle = NULL;
 
     // try transcode's module directory
     if (!handle) {
       // (try 5.x "libdivxencore.so.0" style)
-      snprintf(module, sizeof(module), "%s/%s", path, MODULE_V);
+      sret = snprintf(module, TC_BUF_MAX, "%s/%s", path, MODULE_V);
+      tc_test_string(__FILE__, __LINE__, TC_BUF_MAX, sret, errno);
       handle = dlopen(module, RTLD_LAZY); 
     }
     if (!handle) {
       // (try 4.x "libdivxencore.so" style)
-      snprintf(module, sizeof(module), "%s/%s", path, MODULE);
+      sret = snprintf(module, TC_BUF_MAX, "%s/%s", path, MODULE);
+      tc_test_string(__FILE__, __LINE__, TC_BUF_MAX, sret, errno);
       handle = dlopen(module, RTLD_LAZY); 
     }
 
     //try the default:
     if (!handle) {
       // (try 5.x "libdivxencore.so.0" style)
-      snprintf(module, sizeof(module), "%s", MODULE_V);
+      sret = snprintf(module, TC_BUF_MAX, "%s", MODULE_V);
+      tc_test_string(__FILE__, __LINE__, TC_BUF_MAX, sret, errno);
       handle = dlopen(module, RTLD_LAZY); 
     }
     if (!handle) {
       // (try 4.x "libdivxencore.so" style)
-      snprintf(module, sizeof(module), "%s", MODULE);
+      sret = snprintf(module, TC_BUF_MAX, "%s", MODULE);
+      tc_test_string(__FILE__, __LINE__, TC_BUF_MAX, sret, errno);
       handle = dlopen(module, RTLD_LAZY); 
     }
-    
+
     if (!handle) {
       tc_warn("[%s] %s\n", MOD_NAME, dlerror());
       return(-1);
     } else {  
       if(verbose_flag & TC_DEBUG) 
-        fprintf(stderr, "[%s] Loading external codec module %s\n", MOD_NAME, module); 
+        fprintf(stderr, "[%s] Loading external codec module %s\n",
+                        MOD_NAME, module); 
     }
 
     divx_decore = dlsym(handle, "decore");   
-    
+
     if ((error = dlerror()) != NULL)  {
       tc_warn("[%s] %s\n", MOD_NAME, error);
       return(-1);
@@ -229,12 +235,12 @@ static int divx_init(char *path) {
 
 MOD_open
 {
-  
+
 //  DEC_SET dec_set;
   char *codec_str=NULL;
 
   if(param->flag == TC_VIDEO) {
-    
+
       /*
     if(avifile==NULL) 
       if(NULL == (avifile = AVI_open_input_file(vob->video_in_file,1))){
@@ -242,10 +248,11 @@ MOD_open
 	return(TC_IMPORT_ERROR); 
       }
       */
-    
+
     if(avifile==NULL)  {
       if(vob->nav_seek_file) {
-	if(NULL == (avifile = AVI_open_input_indexfile(vob->video_in_file,0,vob->nav_seek_file))){
+	if(NULL == (avifile = AVI_open_input_indexfile(vob->video_in_file,
+                                                       0,vob->nav_seek_file))){
 	  AVI_print_error("avi open error");
 	  return(TC_IMPORT_ERROR); 
 	} 
@@ -256,7 +263,7 @@ MOD_open
 	} 
       }
     }
-    
+
     // vob->offset contains the last keyframe
     if (!done_seek && vob->vob_offset>0) {
 	//printf("starting decode from last key was (%d)\n", vob->vob_offset);
@@ -264,16 +271,16 @@ MOD_open
 	done_seek=1;
     }
 
-    
+
     //load the codec
-    
+
     if (!decore_in_use) {
       if(divx_init(vob->mod_path)<0) {
 	fprintf(stderr, "[%s] failed to init DivX 4.xx/5.xx codec\n", MOD_NAME);
 	return(TC_IMPORT_ERROR); 
       }
     }
-    
+
     //check DivX version:
     codec_str = AVI_video_compressor(avifile);
 
@@ -310,14 +317,14 @@ MOD_open
       return(TC_IMPORT_ERROR); 
     } else
       memset(divx,0x00,sizeof(DEC_PARAM));
-  
+
     //important parameter
     divx->x_dim = AVI_video_width(avifile);
     divx->y_dim = AVI_video_height(avifile);
     divx->time_incr = 15; //default
 
 
-      if (verbose & TC_DEBUG) 
+      if (verbose & TC_DEBUG)
       		printf("[%s] using DivX5 decoder syntax.\n", MOD_NAME);
 
       if (strcasecmp(codec_str,"DIV3")==0)
@@ -332,8 +339,9 @@ MOD_open
 
     if(strcasecmp(codec_str,"DIV3")==0) {
 	divx_version=DEC_OPT_FRAME_311;
-        if(verbose & TC_DEBUG) 
-		fprintf(stderr, "[%s] detected DivX divx_version3.11 codec\n", MOD_NAME);
+        if(verbose & TC_DEBUG)
+		fprintf(stderr, "[%s] detected DivX divx_version3.11 codec\n",
+                                 MOD_NAME);
     }
     else
 	divx_version=DEC_OPT_FRAME;
@@ -341,9 +349,9 @@ MOD_open
 #endif
 
     codec=vob->im_v_codec;
-    
+
 #if DECORE_VERSION >= 20021112
-    
+
     // yes this only works on little endian machines.
     // Anyway, we don't support divx4linux decoding on big endian machines.
 #define FOURCC(A, B, C, D) ( ((uint8_t) (A)) | (((uint8_t) (B))<<8) | (((uint8_t) (C))<<16) | (((uint8_t) (D))<<24) )
@@ -360,28 +368,28 @@ MOD_open
 
     frame_size = pbi->biWidth * pbi->biHeight * 3;
     switch(codec) {
-      
+
     case CODEC_RGB:
-      
+
       pbi->biCompression=0;
       pbi->biBitCount=24;
       frame_size = pbi->biWidth * pbi->biHeight * 3;
       break;
-      
+
     case CODEC_YUV:
-      
+
       pbi->biCompression = FOURCC('Y','V','1','2');
       frame_size = (pbi->biWidth * pbi->biHeight * 3)/2;
       break;
-      
+
     case CODEC_RAW:
-	
+
       pass_through=1;
       pbi->biCompression = FOURCC('I','4','2','0');
       break;
 
     case CODEC_RAW_YUV:
-      
+
       pass_through=1;
       pass_through_decode=1;
       pbi->biCompression = FOURCC('Y','V','1','2');
@@ -389,31 +397,31 @@ MOD_open
       break;
     }
 
-    
+
 #else
     frame_size = divx->x_dim * divx->y_dim * 3;
     switch(codec) {
-      
+
     case CODEC_RGB:
-      
+
       divx->output_format=DEC_RGB24;
       frame_size = divx->x_dim * divx->y_dim * 3;
       break;
-      
+
     case CODEC_YUV:
-      
+
       divx->output_format=DEC_YV12;
       frame_size = (divx->x_dim * divx->y_dim * 3)/2;
       break;
-      
+
     case CODEC_RAW:
-	
+
       pass_through=1;
       divx->output_format=DEC_420;
       break;
 
     case CODEC_RAW_YUV:
-      
+
       pass_through=1;
       pass_through_decode=1;
       divx->output_format=DEC_YV12;
@@ -421,7 +429,7 @@ MOD_open
       break;
     }
 #endif
-    
+
     //----------------------------------------
     //
     // setup decoder
@@ -439,25 +447,25 @@ MOD_open
       return(TC_IMPORT_ERROR); 
     }
 #else
-    
+
     if(divx_decore(divx_id, DEC_OPT_INIT, divx, NULL) < 0) {
       fprintf(stderr, "[%s] codec DEC_OPT_INIT error", MOD_NAME);
       return(TC_IMPORT_ERROR); 
     } else
 	++decore_in_use;
 #endif
-    
+
     if ((decFrame = malloc(sizeof(DEC_FRAME)))==NULL) {
       perror("out of memory");
       return(TC_IMPORT_ERROR); 
     } else
       memset(decFrame,0x00,sizeof(DEC_FRAME));
- 
+
     if ((decInfo = malloc(sizeof(DEC_FRAME_INFO)))==NULL) {
       perror("out of memory");
       return(TC_IMPORT_ERROR); 
     }
-  
+
     if (!buffer) {
       if ((buffer = bufalloc(frame_size))==NULL) {
 	perror("out of memory");
@@ -467,19 +475,20 @@ MOD_open
 	memset(buffer, 0, frame_size);
       }
     }
-    
+
     //postproc
 /*   XXX enable this somehow. 
     dec_set.postproc_level = vob->quality * 20;  //0-100;
-    if(verbose & TC_DEBUG) printf("[%s] decoder postprocessing quality = %d\n", MOD_NAME, dec_set.postproc_level);
+    if(verbose & TC_DEBUG) printf("[%s] decoder postprocessing quality = %d\n",
+                                   MOD_NAME, dec_set.postproc_level);
 
     divx_decore(divx_id, DEC_OPT_SETPP, &dec_set, NULL);
-  */  
+  */
     param->fd = NULL;
-    
+
     return(0);
   }
-  
+
   return(TC_IMPORT_ERROR);
 }
 
@@ -493,7 +502,7 @@ MOD_open
 MOD_decode {
 
     int key;
-    
+
     long bytes_read=0;
     static char *working_frame = NULL;
 
@@ -506,19 +515,21 @@ MOD_decode {
     }
 
     if(param->flag != TC_VIDEO) return(TC_IMPORT_ERROR);
-    
-    bytes_read = (pass_through) ? AVI_read_frame(avifile, param->buffer, &key):AVI_read_frame(avifile, buffer, &key);
-    
+
+    bytes_read = (pass_through) ?
+        AVI_read_frame(avifile, param->buffer, &key) :
+        AVI_read_frame(avifile, buffer, &key);
+
     if(bytes_read<0) return(TC_IMPORT_ERROR); 
 
     if(pass_through) {
-      
+
       int cc=0;
-      
+
       param->size = (int) bytes_read;
-      
+
       //determine keyframe
-      
+
 #if DECORE_VERSION >= 20020303
 #  if DECORE_VERSION >= 20021112
       switch (decInit->codec_version){
@@ -564,7 +575,7 @@ MOD_decode {
       decFrame->bmp = working_frame;
       decFrame->length = (int) bytes_read;
       decFrame->render_flag = 1;
-      
+
 #if DECORE_VERSION >= 20021112
       decFrame->stride = pbi->biWidth;
       if(divx_decore(dec_handle, divx_version, decFrame, NULL) != DEC_OK) {
@@ -579,20 +590,20 @@ MOD_decode {
 	return(TC_IMPORT_ERROR); 
       }
 #endif
-      
+
       param->size = frame_size;
       tc_memcpy(param->buffer, working_frame, frame_size);
     }
-    
+
     //for preview/pass-through feature
-    
+
     if(pass_through_decode) {
 
       decFrame->bitstream = param->buffer; //read only??
       decFrame->bmp = working_frame; 
       decFrame->length = (int) bytes_read;
       decFrame->render_flag = 1;
-      
+
 #if DECORE_VERSION >= 20021112
       decFrame->stride = pbi->biWidth;
 
@@ -610,7 +621,7 @@ MOD_decode {
 #endif
       tc_memcpy(param->buffer2, working_frame, frame_size);
     }
-    
+
     return(0);
 }
 
@@ -624,7 +635,7 @@ MOD_close
 {  
 
     if(param->flag == TC_VIDEO) {
-	
+
 	int status;
 
 	//fprintf (stderr, "DEBUG [%s:%d] 1\n", __FILE__, __LINE__);
@@ -655,11 +666,10 @@ MOD_close
 	if (avifile)  { AVI_close (avifile); avifile=NULL;  }
 
 	done_seek=0;
- 
+
 	return(0);
     }
 
-    
     return(TC_IMPORT_ERROR);
 }
 
