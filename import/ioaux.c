c8b1dc2244bbeccd40923ad3613688f0c8961ede
Jacob Meuser <jakemsr@sdf.lonestar.org>
2005-05-13 08:42:23 +0000

    - try to allow autoprobing of OSS devices on Linux.  v4l gets in the way   though ...

--- import/ioaux.c
+++ import/ioaux.c
@@ -284,10 +284,13 @@ int probe_path(char *name)
 #else
 	      case 81: /* v4l (Linux) */
                   return(TC_PROBE_PATH_V4L_VIDEO);
+#ifdef HAVE_V4L
 	      case 14: /* dsp (Linux) */
                   return(TC_PROBE_PATH_V4L_AUDIO);
-              case 0: /* OSS */
+#else
+              case 14: /* OSS */
                   return(TC_PROBE_PATH_OSS);
+#endif
 	      default:
 		  break;
 #endif
