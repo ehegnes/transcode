a5c623ec212528ecd8d0738b9292e32380807f11
Jacob Meuser <jakemsr@sdf.lonestar.org>
2005-05-14 03:37:19 +0000

    #include "src/transcode.h"

--- libioaux/configs.c
+++ libioaux/configs.c
@@ -10,15 +10,17 @@
 
 */
 
-#include <errno.h>
+#include "src/transcode.h"
+
+// #include <errno.h>
 #include <limits.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
 #include <ctype.h>
 #include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+// #include <sys/stat.h>
+// #include <unistd.h>
 
 #include "configs.h"
 
