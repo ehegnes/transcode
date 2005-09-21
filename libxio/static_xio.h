0bba499a6b44c968727e2762231cb1b32daeeeac
Francesco Romani <fromani@gmail.com>
2005-09-21 21:31:56 +0000

    silence some compiler warning

--- libxio/static_xio.h
+++ libxio/static_xio.h
@@ -9,15 +9,16 @@ void
 dummy()
 {
 	int i;
+	struct stat tmp;
 
 	i = xio_open("", O_RDONLY);
 	xio_read(i, NULL, 0);
 	xio_write(i, NULL, 0);
 	xio_ftruncate(i, 0);
 	xio_lseek(i, 0, 0);
-	xio_fstat(i, NULL);
-	xio_lstat("", NULL);
-	xio_stat("", NULL);
+	xio_fstat(i, &tmp);
+	xio_lstat("", &tmp);
+	xio_stat("", &tmp);
 	xio_rename("", "");
 	xio_close(i);
 }
