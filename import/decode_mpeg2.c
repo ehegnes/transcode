bf036fa9e431285de7b9cbdbe5cb128b99a4f71b
Andrew Church <achurch@achurch.org>
2005-10-11 09:09:09 +0000

    Fix red/blue swapping bug

--- import/decode_mpeg2.c
+++ import/decode_mpeg2.c
@@ -104,11 +104,11 @@ void decode_mpeg2(decode_t *decode)
 			import_exit (1);
 		}
 		len = sequence->chroma_width * sequence->chroma_height;
-		if (len != p_write (decode->fd_out, info->display_fbuf->buf[2], len)) {
+		if (len != p_write (decode->fd_out, info->display_fbuf->buf[1], len)) {
 			fprintf (stderr, "failed to write U plane of frame");
 			import_exit (1);
 		}
-		if (len != p_write (decode->fd_out, info->display_fbuf->buf[1], len)) {
+		if (len != p_write (decode->fd_out, info->display_fbuf->buf[2], len)) {
 			fprintf (stderr, "failed to write V plane of frame");
 			import_exit (1);
 		}
