3db59bac304288bfb0474f8f2301264d9161b65c
Andrew Church <achurch@achurch.org>
2005-11-08 09:21:21 +0000

    Conform to style guidelines

--- aclib/test-memcpy-speed.c
+++ aclib/test-memcpy-speed.c
@@ -35,69 +35,67 @@ static sigjmp_buf env;
 
 static void sighandler(int sig)
 {
-        printf("*** %s\n", strsignal(sig));
-        siglongjmp(env, 1);
+    printf("*** %s\n", strsignal(sig));
+    siglongjmp(env, 1);
 }
 
 static void set_signals(void)
 {
-        old_SIGSEGV = signal(SIGSEGV, sighandler);
-        old_SIGILL  = signal(SIGILL , sighandler);
+    old_SIGSEGV = signal(SIGSEGV, sighandler);
+    old_SIGILL  = signal(SIGILL , sighandler);
 }
 
 static void clear_signals(void)
 {
-        signal(SIGSEGV, old_SIGSEGV);
-        signal(SIGILL , old_SIGILL );
+    signal(SIGSEGV, old_SIGSEGV);
+    signal(SIGILL , old_SIGILL );
 } 
 
 /* align1 and align2 are 0..63 */
-void testit(void * (*func)(void *, const void *, size_t), int align1, int align2, int length, int * msecs, int * ok)
+void testit(void *(*func)(void *, const void *, size_t),
+            int align1, int align2, int length, int *msecs, int *ok)
 {
-        char * chunk1, * chunk1_base;
-        char * chunk2, * chunk2_base;
-        char * chunkT;
-        int             ix;
-        int             result;
+    char *chunk1, *chunk1_base;
+    char *chunk2, *chunk2_base;
+    char *chunkT;
+    int result;
+
+    chunk1_base = malloc(length+128+spill);
+    chunk2_base = malloc(length+128+spill);
+    chunk1 = (char *)((long)chunk1_base+63 & -64) + align1;
+    chunk2 = (char *)((long)chunk2_base+63 & -64) + align2;
+    memset(chunk1, 0x11, length+spill);
+    memset(chunk2, 0x22, length+spill);
+    chunkT = malloc(length+spill);
+    memset(chunkT, 0x11, length);
+    memset(chunkT+length, 0x22, spill);
+
+    set_signals();
+    if (sigsetjmp(env, 1)) {
+        *ok = 0;
+        *msecs = -1;
+    } else {
+        int ix;
         unsigned long long start, stop;
         struct rusage ru;
 
-        chunk1_base = malloc(length+128+spill);
-        chunk2_base = malloc(length+128+spill);
-        chunk1 = (char *)((long)chunk1_base+63 & -64) + align1;
-        chunk2 = (char *)((long)chunk2_base+63 & -64) + align2;
+        getrusage(RUSAGE_SELF, &ru);
+        start = ru.ru_utime.tv_sec * 1000 + ru.ru_utime.tv_usec / 1000;
+        for (ix = 0; ix < ITERATIONS; ix++)
+            (*func)(chunk2, chunk1, length);
+        getrusage(RUSAGE_SELF, &ru);
+        stop = ru.ru_utime.tv_sec * 1000 + ru.ru_utime.tv_usec / 1000;
+        if (stop == start)
+            stop++;
+        *msecs = stop - start;
 
-        memset(chunk1, 0x11, length+spill);
-        memset(chunk2, 0x22, length+spill);
+        *ok = (memcmp(chunk2, chunkT, length+spill) == 0);
+    }
+    clear_signals();
 
-        chunkT = malloc(length+spill);
-        memset(chunkT, 0x11, length);
-        memset(chunkT+length, 0x22, spill);
-
-        set_signals();
-        if (sigsetjmp(env, 1)) {
-                *ok = 0;
-                *msecs = -1;
-        } else {
-                getrusage(RUSAGE_SELF, &ru);
-                start = ru.ru_utime.tv_sec * 1000 + ru.ru_utime.tv_usec / 1000;
-
-                for(ix = 0; ix < ITERATIONS; ix++)
-                        func(chunk2, chunk1, length);
-
-                getrusage(RUSAGE_SELF, &ru);
-                stop = ru.ru_utime.tv_sec * 1000 + ru.ru_utime.tv_usec / 1000;
-
-                *ok = (memcmp(chunk2, chunkT, length+spill) == 0);
-                if (stop == start)
-                        stop++;
-                *msecs = stop - start;
-        }
-        clear_signals();
-
-        free(chunk1_base);
-        free(chunk2_base);
-        free(chunkT);
+    free(chunk1_base);
+    free(chunk2_base);
+    free(chunkT);
 }
 
 /* Alignments/sizes to test */
@@ -140,19 +138,28 @@ int main(int argc, char ** argv)
     printf("method     ok   msecs  iterations/sec\n");
 
     for (i = 0; tests[i].align1 >= 0; i++) {
-        printf("[%d/%d/%#x]\n", tests[i].align1, tests[i].align2, tests[i].length);
-        testit(memcpy, tests[i].align1, tests[i].align2, tests[i].length, &msecs, &ok);
-        printf("* libc    %-3s %6d  %d\n", ok ? "yes" : "no ", msecs, ITERATIONS / msecs);
+        printf("[%d/%d/%#x]\n", tests[i].align1, tests[i].align2,
+               tests[i].length);
+        testit(memcpy, tests[i].align1, tests[i].align2,
+               tests[i].length, &msecs, &ok);
+        printf("* libc    %-3s %6d  %d\n", ok ? "yes" : "no ",
+               msecs, ITERATIONS / msecs);
 #ifdef ARCH_X86
-        testit(memcpy_mmx, tests[i].align1, tests[i].align2, tests[i].length, &msecs, &ok);
-        printf("* mmx     %-3s %6d  %d\n", ok ? "yes" : "no ", msecs, ITERATIONS / msecs);
-        testit(memcpy_sse, tests[i].align1, tests[i].align2, tests[i].length, &msecs, &ok);
-        printf("* sse     %-3s %6d  %d\n", ok ? "yes" : "no ", msecs, ITERATIONS / msecs);
+        testit(memcpy_mmx, tests[i].align1, tests[i].align2,
+               tests[i].length, &msecs, &ok);
+        printf("* mmx     %-3s %6d  %d\n", ok ? "yes" : "no ",
+               msecs, ITERATIONS / msecs);
+        testit(memcpy_sse, tests[i].align1, tests[i].align2,
+               tests[i].length, &msecs, &ok);
+        printf("* sse     %-3s %6d  %d\n", ok ? "yes" : "no ",
+               msecs, ITERATIONS / msecs);
 #endif
 #if defined(ARCH_X86_64)
-        testit(memcpy_amd64, tests[i].align1, tests[i].align2, tests[i].length, &msecs, &ok);
-        printf("* amd64   %-3s %6d  %d\n", ok ? "yes" : "no ", msecs, ITERATIONS / msecs);
+        testit(memcpy_amd64, tests[i].align1, tests[i].align2,
+               tests[i].length, &msecs, &ok);
+        printf("* amd64   %-3s %6d  %d\n", ok ? "yes" : "no ",
+               msecs, ITERATIONS / msecs);
 #endif
     }
-    return(0);
+    return 0;
 }
--- aclib/test-memcpy.c
+++ aclib/test-memcpy.c
@@ -28,69 +28,55 @@ static void *old_SIGSEGV = NULL, *old_SIGILL = NULL;
 
 static void sighandler(int sig)
 {
-        printf("*** %s\n", strsignal(sig));
-        siglongjmp(env, 1);
+    printf("*** %s\n", strsignal(sig));
+    siglongjmp(env, 1);
 }
 
 static void set_signals(void)
 {
-        old_SIGSEGV = signal(SIGSEGV, sighandler);
-        old_SIGILL  = signal(SIGILL , sighandler);
+    old_SIGSEGV = signal(SIGSEGV, sighandler);
+    old_SIGILL  = signal(SIGILL , sighandler);
 }
 
 static void clear_signals(void)
 {
-        signal(SIGSEGV, old_SIGSEGV);
-        signal(SIGILL , old_SIGILL );
+    signal(SIGSEGV, old_SIGSEGV);
+    signal(SIGILL , old_SIGILL );
 } 
 
 /* align1 and align2 are 0..63 */
 int testit(void *(*func)(void *, const void *, size_t), int align1, int align2, int length)
 {
-        char * chunk1, * chunk1_base;
-        char * chunk2, * chunk2_base;
-        char * chunkT;
-//      int             ix;
-        int             result;
-//      unsigned long long start, stop;
-//      struct rusage ru;
+    char *chunk1, *chunk1_base;
+    char *chunk2, *chunk2_base;
+    char *chunkT;
+    int result;
 
-        chunk1_base = malloc(length+128+spill);
-        chunk2_base = malloc(length+128+spill);
-        chunk1 = (char *)((long)chunk1_base+63 & -64) + align1;
-        chunk2 = (char *)((long)chunk2_base+63 & -64) + align2;
+    chunk1_base = malloc(length+128+spill);
+    chunk2_base = malloc(length+128+spill);
+    chunk1 = (char *)((long)chunk1_base+63 & -64) + align1;
+    chunk2 = (char *)((long)chunk2_base+63 & -64) + align2;
 
-        memset(chunk1, 0x11, length+spill);
-        memset(chunk2, 0x22, length+spill);
+    memset(chunk1, 0x11, length+spill);
+    memset(chunk2, 0x22, length+spill);
 
-        chunkT = malloc(length+spill);
-        memset(chunkT, 0x11, length);
-        memset(chunkT+length, 0x22, spill);
+    chunkT = malloc(length+spill);
+    memset(chunkT, 0x11, length);
+    memset(chunkT+length, 0x22, spill);
 
-        set_signals();
-        if (sigsetjmp(env, 1)) {
-                result = 0;
-        } else {
-//              getrusage(RUSAGE_SELF, &ru);
-//              start = ru.ru_utime.tv_sec * 1000 + ru.ru_utime.tv_usec / 1000;
+    set_signals();
+    if (sigsetjmp(env, 1)) {
+        result = 0;
+    } else {
+        (*func)(chunk2, chunk1, length);
+        result = (memcmp(chunk2, chunkT, length+spill) == 0);
+    }
+    clear_signals();
 
-//              for(ix = 0; ix < it; ix++)
-                        func(chunk2, chunk1, length);
-
-//              getrusage(RUSAGE_SELF, &ru);
-//              stop = ru.ru_utime.tv_sec * 1000 + ru.ru_utime.tv_usec / 1000;
-
-                result = (memcmp(chunk2, chunkT, length+spill) == 0);
-//              if (stop == start)
-//                      stop++;
-//              *msecs = stop - start;
-        }
-        clear_signals();
-
-        free(chunk1_base);
-        free(chunk2_base);
-        free(chunkT);
-        return result;
+    free(chunk1_base);
+    free(chunk2_base);
+    free(chunkT);
+    return result;
 }
 
 int main(int ac, char **av)
