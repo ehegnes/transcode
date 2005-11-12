7507125cbd00100deff28424e1dab99fc1e60305
Andrew Church <achurch@achurch.org>
2005-11-12 11:43:54 +0000

    Fix off-by-one bug in accelerated average functions

--- aclib/average.c
+++ aclib/average.c
@@ -37,8 +37,6 @@ static void average(const uint8_t *src1, const uint8_t *src2,
 
 #ifdef ARCH_X86
 
-/* MMX-optimized */
-
 static void average_mmx(const uint8_t *src1, const uint8_t *src2,
                         uint8_t *dest, int bytes)
 {
@@ -49,11 +47,11 @@ static void average_mmx(const uint8_t *src1, const uint8_t *src2,
             pcmpeqw %%mm5, %%mm5                                        \n\
             psubw %%mm5, %%mm6          # Put 0x0001*4 in MM6           \n\
             0:                                                          \n\
-            movq (%%esi,%%eax), %%mm0                                   \n\
+            movq -8(%%esi,%%eax), %%mm0                                 \n\
             movq %%mm0, %%mm1                                           \n\
             punpcklbw %%mm7, %%mm0                                      \n\
             punpckhbw %%mm7, %%mm1                                      \n\
-            movq (%%edx,%%eax), %%mm2                                   \n\
+            movq -8(%%edx,%%eax), %%mm2                                 \n\
             movq %%mm2, %%mm3                                           \n\
             punpcklbw %%mm7, %%mm2                                      \n\
             punpckhbw %%mm7, %%mm3                                      \n\
@@ -64,12 +62,12 @@ static void average_mmx(const uint8_t *src1, const uint8_t *src2,
             paddw %%mm6, %%mm1                                          \n\
             psrlw $1, %%mm1                                             \n\
             packuswb %%mm1, %%mm0                                       \n\
-            movq %%mm0, (%%edi,%%eax)                                   \n\
+            movq %%mm0, -8(%%edi,%%eax)                                 \n\
             subl $8, %%eax                                              \n\
             jnz 0b                                                      \n\
             emms"
             : /* no outputs */
-            : "S" (src1), "d" (src2), "D" (dest), "a" ((bytes & ~7) - 8));
+            : "S" (src1), "d" (src2), "D" (dest), "a" (bytes & ~7));
     }
     if (UNLIKELY(bytes & 7)) {
         average(src1+(bytes & ~7), src2+(bytes & ~7), dest+(bytes & ~7),
@@ -78,7 +76,7 @@ static void average_mmx(const uint8_t *src1, const uint8_t *src2,
 }
 
 
-/* SSE-optimized (PAVGB available) */
+/* SSE has PAVGB */
 
 static void average_sse(const uint8_t *src1, const uint8_t *src2,
                         uint8_t *dest, int bytes)
@@ -88,39 +86,39 @@ static void average_sse(const uint8_t *src1, const uint8_t *src2,
             testl $~0x1F, %%eax                                         \n\
             jz 1f                                                       \n\
             0:                                                          \n\
-            movq (%%esi,%%eax), %%mm0                                   \n\
-            movq 8(%%esi,%%eax), %%mm1                                  \n\
-            movq 16(%%esi,%%eax), %%mm2                                 \n\
-            movq 24(%%esi,%%eax), %%mm3                                 \n\
-            movq (%%edx,%%eax), %%mm4                                   \n\
+            movq -32(%%esi,%%eax), %%mm0                                \n\
+            movq -24(%%esi,%%eax), %%mm1                                \n\
+            movq -16(%%esi,%%eax), %%mm2                                \n\
+            movq -8(%%esi,%%eax), %%mm3                                 \n\
+            movq -32(%%edx,%%eax), %%mm4                                \n\
             pavgb %%mm4, %%mm0                                          \n\
-            movq 8(%%edx,%%eax), %%mm5                                  \n\
+            movq -24(%%edx,%%eax), %%mm5                                \n\
             pavgb %%mm5, %%mm1                                          \n\
-            movq 16(%%edx,%%eax), %%mm6                                 \n\
+            movq -16(%%edx,%%eax), %%mm6                                \n\
             pavgb %%mm6, %%mm2                                          \n\
-            movq 24(%%edx,%%eax), %%mm7                                 \n\
+            movq -8(%%edx,%%eax), %%mm7                                 \n\
             pavgb %%mm7, %%mm3                                          \n\
-            movntq %%mm0, (%%edi,%%eax)                                 \n\
-            movntq %%mm1, 8(%%edi,%%eax)                                \n\
-            movntq %%mm2, 16(%%edi,%%eax)                               \n\
-            movntq %%mm3, 24(%%edi,%%eax)                               \n\
+            movntq %%mm0, -32(%%edi,%%eax)                              \n\
+            movntq %%mm1, -24(%%edi,%%eax)                              \n\
+            movntq %%mm2, -16(%%edi,%%eax)                              \n\
+            movntq %%mm3, -8(%%edi,%%eax)                               \n\
             subl $32, %%eax                                             \n\
             testl $~0x1F, %%eax                                         \n\
             jnz 0b                                                      \n\
             testl %%eax, %%eax                                          \n\
             jz 2f                                                       \n\
             1:                                                          \n\
-            movq (%%esi,%%eax), %%mm0                                   \n\
-            movq (%%edx,%%eax), %%mm1                                   \n\
+            movq -8(%%esi,%%eax), %%mm0                                 \n\
+            movq -8(%%edx,%%eax), %%mm1                                 \n\
             pavgb %%mm1, %%mm0                                          \n\
-            movntq %%mm0, (%%edi,%%eax)                                 \n\
+            movntq %%mm0, -8(%%edi,%%eax)                               \n\
             subl $8, %%eax                                              \n\
             jnz 1b                                                      \n\
             2:                                                          \n\
             emms                                                        \n\
             sfence"
             : /* no outputs */
-            : "S" (src1), "d" (src2), "D" (dest), "a" ((bytes & ~7) - 8));
+            : "S" (src1), "d" (src2), "D" (dest), "a" (bytes & ~7));
     }
     if (UNLIKELY(bytes & 7)) {
         average(src1+(bytes & ~7), src2+(bytes & ~7), dest+(bytes & ~7),
@@ -154,40 +152,40 @@ static void average_sse2(const uint8_t *src1, const uint8_t *src2,
             testl $~0x3F, %%eax                                         \n\
             jz 1f                                                       \n\
             0:                                                          \n\
-            movdqu ("ESI","EAX"), %%xmm0                                \n\
-            movdqu 16("ESI","EAX"), %%xmm1                              \n\
-            movdqu 32("ESI","EAX"), %%xmm2                              \n\
-            movdqu 48("ESI","EAX"), %%xmm3                              \n\
-            movdqu ("EDX","EAX"), %%xmm4                                \n\
+            movdqu -64("ESI","EAX"), %%xmm0                             \n\
+            movdqu -48("ESI","EAX"), %%xmm1                             \n\
+            movdqu -32("ESI","EAX"), %%xmm2                             \n\
+            movdqu -16("ESI","EAX"), %%xmm3                             \n\
+            movdqu -64("EDX","EAX"), %%xmm4                             \n\
             pavgb %%xmm4, %%xmm0                                        \n\
-            movdqu 16("EDX","EAX"), %%xmm5                              \n\
+            movdqu -48("EDX","EAX"), %%xmm5                             \n\
             pavgb %%xmm5, %%xmm1                                        \n\
-            movdqu 32("EDX","EAX"), %%xmm6                              \n\
+            movdqu -32("EDX","EAX"), %%xmm6                             \n\
             pavgb %%xmm6, %%xmm2                                        \n\
-            movdqu 48("EDX","EAX"), %%xmm7                              \n\
+            movdqu -16("EDX","EAX"), %%xmm7                             \n\
             pavgb %%xmm7, %%xmm3                                        \n\
             # Note that movntdq requires 16-byte alignment, which we're \n\
             # not guaranteed                                            \n\
-            movdqu %%xmm0, ("EDI","EAX")                                \n\
-            movdqu %%xmm1, 16("EDI","EAX")                              \n\
-            movdqu %%xmm2, 32("EDI","EAX")                              \n\
-            movdqu %%xmm3, 48("EDI","EAX")                              \n\
+            movdqu %%xmm0, -64("EDI","EAX")                             \n\
+            movdqu %%xmm1, -48("EDI","EAX")                             \n\
+            movdqu %%xmm2, -32("EDI","EAX")                             \n\
+            movdqu %%xmm3, -16("EDI","EAX")                             \n\
             subl $64, %%eax                                             \n\
             testl $~0x3F, %%eax                                         \n\
             jnz 0b                                                      \n\
             testl %%eax, %%eax                                          \n\
             jz 2f                                                       \n\
             1:                                                          \n\
-            movq ("ESI","EAX"), %%mm0                                   \n\
-            movq ("EDX","EAX"), %%mm1                                   \n\
+            movq -8("ESI","EAX"), %%mm0                                 \n\
+            movq -8("EDX","EAX"), %%mm1                                 \n\
             pavgb %%mm1, %%mm0                                          \n\
-            movq %%mm0, ("EDI","EAX")                                   \n\
+            movq %%mm0, -8("EDI","EAX")                                 \n\
             subl $8, %%eax                                              \n\
             jnz 1b                                                      \n\
             2:                                                          \n\
             emms"
             : /* no outputs */
-            : "S" (src1), "d" (src2), "D" (dest), "a" ((bytes & ~7) - 8));
+            : "S" (src1), "d" (src2), "D" (dest), "a" (bytes & ~7));
     }
     if (UNLIKELY(bytes & 7)) {
         average(src1+(bytes & ~7), src2+(bytes & ~7), dest+(bytes & ~7),
