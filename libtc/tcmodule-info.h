3da870a796b3d28c1d0a332eef522d6abf194508
Francesco Romani <fromani@gmail.com>
2005-12-12 13:42:41 +0000

    Update extended capabilities code and adapt it to incoming introduction of new module system code.

---
+++ libtc/tcmodule-info.h
@@ -0,0 +1,181 @@
+/*
+ * tcmodule-info.h - module data (capabilities) and helper functions
+ * (C) 2005 - Francesco Romani <fromani -at- gmail -dot- com>
+ *
+ * This file is part of transcode, a video stream processing tool.
+ * transcode is free software, distributable under the terms of the GNU
+ * General Public License (version 2 or later).  See the file COPYING
+ * for details.
+ */
+
+#ifndef TCMODULEINFO_H
+#define TCMODULEINFO_H
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+
+/* FIXME: move to a enum? */
+/* actions */
+#define TC_MODULE_FEATURE_NONE          0x00000000
+#define TC_MODULE_FEATURE_FILTER        0x00000001
+#define TC_MODULE_FEATURE_DECODE        0x00000002
+#define TC_MODULE_FEATURE_ENCODE        0x00000004
+#define TC_MODULE_FEATURE_DEMULTIPLEX   0x00000020
+#define TC_MODULE_FEATURE_MULTIPLEX     0x00000040
+/* targets */
+#define TC_MODULE_FEATURE_VIDEO         0x00010000
+#define TC_MODULE_FEATURE_AUDIO         0x00020000
+#define TC_MODULE_FEATURE_EXTRA         0x00040000
+
+#define TC_MODULE_FLAG_NONE             0x00000000
+#define TC_MODULE_FLAG_RECONFIGURABLE   0x00000001 
+/* module can be reconfigured multiple times safely? */
+
+/*
+ * this structure will hold all the interesting informations
+ * both for user and for transcode itself of a given module.
+ */
+typedef struct tcmoduleinfo_ TCModuleInfo;
+struct tcmoduleinfo_ {
+        uint32_t features; /* what this module can do? */    
+        uint32_t flags; /* quirks */
+        
+        const char *name;
+        const char *version;
+	const char *description;
+
+        /* 
+         * the following two MUST point to an array of TC_CODEC_*
+         * terminated by a TC_CODEC_ERROR value
+         */
+        int *codecs_in;
+	int *codecs_out;
+};
+
+/*
+ * tc_module_info_match: 
+ *     scan the given informations about two modules, and tell if the 
+ *     two modules can be chained toghether.
+ *
+ * Parameters: 
+ *     head: 
+ *         the first given module information structure;
+ *         'head' output is supposed to fit in 'tail' input.
+ *     tail: 
+ *         the second  given module information structure;
+ *         tail' input is supposed to be given by 'head' output.
+ *         
+ * Return value: 
+ *     1 if 'head' can feed 'tail' safely,
+ *     0 otherwise
+ *     
+ * Side effects: 
+ *     none
+ *
+ * Preconditions: 
+ *     none
+ *
+ * Postconditions: 
+ *     none
+ */
+int tc_module_info_match(const TCModuleInfo *head,
+                         const TCModuleInfo *tail);
+
+/*
+ * tc_module_info_log: 
+ *     pretty-print the content of a given module information structure 
+ *     using functions of tc_log_*() family.
+ *
+ * Parameters: 
+ *     info: 
+ *         module information structure to dump
+ *     verbose: 
+ *         level of detail of description, ranging to TC_QUIET to TC_STATS. 
+ *         Other values will be ignored
+ *         
+ * Return value: 
+ *     none
+ *
+ * Side effects: 
+ *     some informations are printed using tc_log_*()
+ *     
+ * Preconditions: 
+ *     'verbose' must be in range TC_QUIET ... TC_STATS
+ *
+ * Postconditions: 
+ *     none
+ */
+void tc_module_info_log(const TCModuleInfo *info, int verbose);
+
+/*
+ * tc_module_info_copy:
+ *     create an exact copy of 'src' in 'dst', allocating new fields.
+ *     PLEASE NOTE: this is an 'hard' (real) copy, which makes two
+ *     TCModuleInfo identical but indipendent.
+ *     For a 'soft' (reference) copy, just memcpy() the two structures.
+ *
+ * Parameters:
+ *     src: TCModuleInfo structure to be copied
+ *     dst: pointer to structure that will hold the copy.
+ *         
+ * Return value: 
+ *     0  successfull
+ *     -1 given (at least) a bad TCModuleInfo reference
+ *     1  not enough memory to perform a full copy
+ *
+ * Side effects: 
+ *     memory in 'dst' is allocated, usinc tc_* helpers, to hold data 
+ *     provided by 'src'.
+ *     
+ * Preconditions: 
+ *     'src' and 'dst' must point to valid TCModuleInfo structures.
+ *
+ * Postconditions: 
+ *     'dst' is an exact copy of 'src'.
+ */
+int tc_module_info_copy(const TCModuleInfo *src, TCModuleInfo *dst);
+
+/*
+ * tc_module_info_free:
+ *     free all resources (memory) acquired when copying a TCModuleInfo
+ *     structure into another one.
+ *
+ * Parameters:
+ *     info: structure copied to be freed
+ *
+ * Return value:
+ *     none
+ *
+ * Side effects:
+ *     none
+ * 
+ * Preconditions:
+ *     'info' must be obtained as result of 'tc_module_info_copy' function,
+ *     as 'dst' parameter. Applying this function to a structure obtained
+ *     in a different way will cause an undefined behaviour, most likely
+ *     a memory corruption or a crash
+ *
+ * Postconditions:
+ *     all resources (memory) acquired by 'info' are released.
+ *     
+ */
+void tc_module_info_free(TCModuleInfo *info);
+
+/*************************************************************************/
+
+#endif  /* TCMODULEINFO_H */
+
+/*
+ * Local variables:
+ *   c-file-style: "stroustrup"
+ *   c-file-offsets: ((case-label . *) (statement-case-intro . *))
+ *   indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+ */
--- libtc/tcmoduleinfo.c
+++ libtc/tcmoduleinfo.c
@@ -1,6 +1,6 @@
 /*
  * tcmoduleinfo.c - module informations (capabilities) and helper functions
- * Written by Francesco Romani <fromani -at- gmail -dot- com>
+ * (C) 2005 - Francesco Romani <fromani -at- gmail -dot- com>
  *
  * This file is part of transcode, a video stream processing tool.
  * transcode is free software, distributable under the terms of the GNU
@@ -12,7 +12,7 @@
 #include "src/transcode.h"
 #include "libtc.h"
 #include "tccodecs.h"
-#include "tcmoduleinfo.h"
+#include "tcmodule-info.h"
 
 int tc_module_info_match(const TCModuleInfo *head, const TCModuleInfo *tail)
 {
@@ -24,16 +24,11 @@ int tc_module_info_match(const TCModuleInfo *head, const TCModuleInfo *tail)
         return 0;
     }
 
-    /* API mismatch, pretty useless now */
-    if (head->api_version != tail->api_version) {
-        return 0;
-    }
-    
     /* 
      * a multiplexor module can be chained with nothing, 
      * it must be placed at the end of the chain
      */
-    if (head->flags & TC_MODULE_MULTIPLEX) {
+    if (head->features & TC_MODULE_FEATURE_MULTIPLEX) {
         return 0;
     }
     
@@ -93,42 +88,119 @@ void tc_module_info_log(const TCModuleInfo *info, int verbose)
         return;
     }
 
-    if (verbose >= TC_INFO) {
-        tc_log_info(info->name, "version %s (API version %i)", 
-                    info->version, info->api_version);
+    if (verbose >= TC_STATS) {
+        tc_log_info(info->name, "description:\n%s", info->description);
     }
 
     if (verbose >= TC_DEBUG) {
-        tc_log_info(info->name, "description:\n%s", info->description);
-
-        if (info->flags == TC_MODULE_NOTHING) {
+        if (info->features == TC_MODULE_FEATURE_NONE) {
             strlcpy(buffer, "none (this shouldn't happen!", DATA_BUF_SIZE);
         } else {
             tc_snprintf(buffer, DATA_BUF_SIZE, "%s%s%s", 
-                        (info->flags & TC_MODULE_VIDEO) ?"video " :"",
-                        (info->flags & TC_MODULE_AUDIO) ?"audio " :"",
-                        (info->flags & TC_MODULE_EXTRA) ?"extra" :"");
+                  (info->features & TC_MODULE_FEATURE_VIDEO) ?"video " :"",
+                  (info->features & TC_MODULE_FEATURE_AUDIO) ?"audio " :"",
+                  (info->features & TC_MODULE_FEATURE_EXTRA) ?"extra" :"");
         }
         tc_log_info(info->name, "can handle : %s", buffer);
 
-        if (info->flags == TC_MODULE_NONE) {
+        if (info->features == TC_MODULE_FEATURE_NONE) {
             strlcpy(buffer, "nothing (this shouldn't happen!", DATA_BUF_SIZE);
         } else {
-            tc_snprintf(buffer, DATA_BUF_SIZE, "%s%s", 
-                        (info->flags & TC_MODULE_ENCODE) ?"encoding " :"",
-                        (info->flags & TC_MODULE_MULTIPLEX) ?"multiplexing" :"");
+            tc_snprintf(buffer, DATA_BUF_SIZE, "%s%s%s", 
+                        (info->features & TC_MODULE_FEATURE_FILTER) 
+                            ?"filtering " :"",
+                        (info->features & TC_MODULE_FEATURE_ENCODE) 
+                            ?"encoding " :"",
+                        (info->features & TC_MODULE_FEATURE_MULTIPLEX) 
+                            ?"multiplexing" :"");
         }
         tc_log_info(info->name, "can do     : %s", buffer);
     }
 
-    if (verbose >= TC_STATS) {
-        const char *str = (info->flags & TC_MODULE_MULTIPLEX) 
+    if (verbose >= TC_INFO) {
+        const char *str = (info->features & TC_MODULE_FEATURE_MULTIPLEX)
                                     ?"a media stream" :"nothing";
         codecs_to_string(info->codecs_in, buffer, DATA_BUF_SIZE, str);
         tc_log_info(info->name, "accepts    : %s", buffer);
 
         codecs_to_string(info->codecs_out, buffer, DATA_BUF_SIZE, str);
         tc_log_info(info->name, "produces   : %s", buffer);
+    }
+}
+
+int tc_module_info_copy(const TCModuleInfo *src, TCModuleInfo *dst)
+{
+    int i = 0;
+    
+    if (!src || !dst) {
+        return -1;
+    }
+    dst->features = src->features;
+    dst->flags = src->flags;
+    
+    dst->name = tc_strdup(src->name);
+    if (!dst->name) {
+        goto no_mem_name;
+    }
+    dst->version = tc_strdup(src->version);
+    if (!dst->version) {
+        goto no_mem_version;
+    }
+    dst->description = tc_strdup(src->description);
+    if (!dst->description) {
+        goto no_mem_description;
+    }
+
+    for (i = 0; src->codecs_in[i] != TC_CODEC_ERROR; i++) {
+        ; /* do nothing */
+    }
+    i++; /* for end mark (TC_CODEC_ERROR) */
+    dst->codecs_in = tc_malloc(i * sizeof(int));
+    if (!dst->codecs_in) {
+        goto no_mem_codecs_in;
+    }
+    memcpy(dst->codecs_in, src->codecs_in, i * sizeof(int));
+ 
+    for (i = 0; src->codecs_out[i] != TC_CODEC_ERROR; i++) {
+        ; /* do nothing */
+    }
+    i++; /* for end mark (TC_CODEC_ERROR) */
+    dst->codecs_out = tc_malloc(i * sizeof(int));
+    if (!dst->codecs_out) {
+        goto no_mem_codecs_out;
+    }
+    memcpy(dst->codecs_out, src->codecs_out, i * sizeof(int));
+    
+    return 0;
+    
+    /* 
+     * void* casts to silence warning from gcc 4.x (free requires void*,
+     * argument is const something* 
+     */
+no_mem_codecs_out:
+    tc_free((void*)dst->codecs_in);
+no_mem_codecs_in:
+    tc_free((void*)dst->description);
+no_mem_description:
+    tc_free((void*)dst->version);
+no_mem_version:
+    tc_free((void*)dst->name);
+no_mem_name:
+    return 1;
+}
+
+void tc_module_info_free(TCModuleInfo *info)
+{
+    if (info != NULL) {
+        /* 
+         * void* casts to silence warning from gcc 4.x (free requires void*,
+         * argument is const something* 
+         */
+        tc_free((void*)info->name);
+        tc_free((void*)info->version);
+        tc_free((void*)info->description);
+        tc_free((void*)info->codecs_in);
+        tc_free((void*)info->codecs_out);
     }
 }
 
@@ -139,21 +211,25 @@ BEGIN_TEST_CODE
 
 // compile command: gcc -Wall -g -O -I. -I.. source.c path/to/libtc.a
 #include "src/transcode.h"
-#include "tcmoduleinfo.h"
+#include "tcmodule-info.h"
 #include "tccodecs.h"
 
 static const int empty_codecs[] = { TC_CODEC_ERROR };
 static TCModuleInfo empty = {
-    TC_MODULE_NONE, 0, 0,
-    "", "", "",
+    TC_MODULE_FEATURE_NONE,
+    TC_MODULE_FLAG_NONE,
+    "",
+    "",
+    "",
     empty_codecs,
     empty_codecs
 };
 
 static const int pass_enc_codecs[] = { TC_CODEC_ANY, TC_CODEC_ERROR };
 static TCModuleInfo pass_enc = {
-    TC_MODULE_ENCODE | TC_MODULE_VIDEO | TC_MODULE_AUDIO | TC_MODULE_EXTRA, 
-    0, 0,
+    TC_MODULE_FEATURE_ENCODE | TC_MODULE_FEATURE_VIDEO 
+        | TC_MODULE_FEATURE_AUDIO | TC_MODULE_FEATURE_EXTRA, 
+    TC_MODULE_FLAG_RECONFIGURABLE,
     "encode_pass.so",
     "0.0.1 (2005-11-14)",
     "accepts everything, outputs verbatim",
@@ -163,8 +239,9 @@ static TCModuleInfo pass_enc = {
 
 static const int fake_mplex_codecs[] = { TC_CODEC_ANY, TC_CODEC_ERROR };
 static TCModuleInfo fake_mplex = {
-    TC_MODULE_MULTIPLEX | TC_MODULE_VIDEO | TC_MODULE_AUDIO | TC_MODULE_EXTRA, 
-    0, 0,
+    TC_MODULE_FEATURE_MULTIPLEX | TC_MODULE_FEATURE_VIDEO 
+        | TC_MODULE_FEATURE_AUDIO | TC_MODULE_FEATURE_EXTRA, 
+    TC_MODULE_FLAG_RECONFIGURABLE,
     "mplex_null.so",
     "0.0.1 (2005-11-14)",
     "accepts and discards everything",
@@ -175,7 +252,8 @@ static TCModuleInfo fake_mplex = {
 static const int fake_mpeg_codecs_in[] = { TC_CODEC_YUV420P, TC_CODEC_ERROR };
 static const int fake_mpeg_codecs_out[] = { TC_CODEC_MPEG1, TC_CODEC_MPEG2, TC_CODEC_XVID, TC_CODEC_ERROR };
 static TCModuleInfo fake_mpeg_enc = {
-    TC_MODULE_ENCODE | TC_MODULE_VIDEO, 0, 0,
+    TC_MODULE_FEATURE_ENCODE | TC_MODULE_FEATURE_VIDEO,
+    TC_MODULE_FLAG_NONE
     "encode_mpeg.so",
     "0.0.1 (2005-11-14)",
     "fake YUV420P -> MPEG video encoder",
@@ -186,7 +264,8 @@ static TCModuleInfo fake_mpeg_enc = {
 static const int fake_vorbis_codecs_in[] = { TC_CODEC_PCM, TC_CODEC_ERROR };
 static const int fake_vorbis_codecs_out[] = { TC_CODEC_VORBIS, TC_CODEC_ERROR };
 static TCModuleInfo fake_vorbis_enc = {
-    TC_MODULE_ENCODE | TC_MODULE_AUDIO, 0, 0,
+    TC_MODULE_FEATURE_ENCODE | TC_MODULE_FEATURE_AUDIO,
+    TC_MODULE_FLAG_NONE,
     "encode_vorbis.so",
     "0.0.1 (2005-11-14)",
     "fake PCM -> Vorbis audio encoder",
@@ -196,7 +275,9 @@ static TCModuleInfo fake_vorbis_enc = {
 
 static const int fake_avi_codecs_in[] = { TC_CODEC_MPEG1, TC_CODEC_XVID, TC_CODEC_MP3, TC_CODEC_ERROR };
 static TCModuleInfo fake_avi_mplex = {
-    TC_MODULE_MULTIPLEX | TC_MODULE_VIDEO | TC_MODULE_AUDIO, 0, 0,
+    TC_MODULE_FEATURE_MULTIPLEX | TC_MODULE_FEATURE_VIDEO 
+        | TC_MODULE_FEATURE_AUDIO,
+    TC_MODULE_FLAG_NONE,
     "mplex_avi.so",
     "0.0.1 (2005-11-14)",
     "fakes an AVI muxer",
