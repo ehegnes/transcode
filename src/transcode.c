7bcfc460019430ff816fc853efc3f292ed53ba3e
Francesco Romani <fromani@gmail.com>
2006-03-06 20:46:37 +0000

    src/transcode.c: switch back to old style encoder (and update API accordingly). tools/Makefile.am: enable new style code.

--- src/transcode.c
+++ src/transcode.c
@@ -544,11 +544,11 @@ static int transcoder(int mode, vob_t *vob)
       if(ex_vid_mod && strcmp(ex_vid_mod,"null") != 0) tc_encode_stream|=TC_VIDEO;
 
       // load export modules and check capabilities
-      if(export_init(tc_builtin_buffer(vob, -1), NULL)<0) {
+      if(export_init(tc_builtin_buffer(vob, TC_FRAME_LAST), NULL)<0) {
       	tc_error("failed to init export layer");
 	return(-1);
       }
-      if(export_setup(ex_aud_mod, ex_vid_mod, NULL)<0) {
+      if(export_setup(vob, ex_aud_mod, ex_vid_mod)<0) {
       	tc_error("failed to init export modules");
 	return(-1);
       }
