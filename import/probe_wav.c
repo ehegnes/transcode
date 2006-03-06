5a6573d651d1c7eff03b1f5fe5f86ecf0aac2638
Francesco Romani <fromani@gmail.com>
2006-03-06 08:06:19 +0000

    switch to wavlib.

--- import/probe_wav.c
+++ import/probe_wav.c
@@ -25,30 +25,33 @@
 #include "tcinfo.h"
 #include "ioaux.h"
 #include "tc.h"
-#include "avilib.h"
+#include "wavlib.h"
 
 void probe_wav(info_t *ipipe)
 {
+    WAVError err = 0;
+    WAV wav = NULL;
+    int chans = 0;
 
-  struct wave_header rtf;
+    wav = wav_fdopen(ipipe->fd_in, WAV_READ, &err);
+    if (!wav) {
+        fprintf(stderr, "(%s) %s\n", __FILE__, wav_strerror(err));
+        ipipe->error = 1;
+        return;
+    }
 
-  // read frame
-  if(AVI_read_wave_header(ipipe->fd_in, &rtf) != 0) {
-     fprintf(stderr, "(%s) %s\n", __FILE__, AVI_strerror());
-     ipipe->error=1;
-     return;
-  }
+    chans = wav_get_channels(wav);
+    ipipe->probe_info->track[0].chan = chans;
+    ipipe->probe_info->track[0].samplerate = wav_get_rate(wav);
+    ipipe->probe_info->track[0].bits = wav_get_bits(wav);
+    ipipe->probe_info->track[0].bitrate = wav_get_bitrate(wav);
+    ipipe->probe_info->track[0].format = 0x1; /* XXX */
 
-  ipipe->probe_info->track[0].samplerate = rtf.common.dwSamplesPerSec;
-  ipipe->probe_info->track[0].chan = rtf.common.wChannels;
-  ipipe->probe_info->track[0].bits = rtf.common.wBitsPerSample;
-  ipipe->probe_info->track[0].bitrate = rtf.common.dwAvgBytesPerSec*8/1000;
-  ipipe->probe_info->track[0].format = 0x1;
+    ipipe->probe_info->magic = TC_MAGIC_WAV;
+    ipipe->probe_info->codec = TC_CODEC_PCM;
 
-  ipipe->probe_info->magic = TC_MAGIC_WAV;
-  ipipe->probe_info->codec = TC_CODEC_PCM;
-
-  if(ipipe->probe_info->track[0].chan>0) ipipe->probe_info->num_tracks=1;
-
-  return;
+    if (ipipe->probe_info->track[0].chan > 0) {
+        ipipe->probe_info->num_tracks = 1;
+    }
+    return;
 }
