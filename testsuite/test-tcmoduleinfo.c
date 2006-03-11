626f64df1b450ad4018551f857dd295d49869fec
Francesco Romani <fromani@gmail.com>
2006-03-11 14:09:42 +0000

    update test to use new tcmoduleinfo interface.

--- testsuite/test-tcmoduleinfo.c
+++ testsuite/test-tcmoduleinfo.c
@@ -52,7 +52,7 @@ static TCModuleInfo fake_mplex = {
 };
 
 static const int fake_mpeg_codecs_in[] = { TC_CODEC_YUV420P, TC_CODEC_ERROR };
-static const int fake_mpeg_codecs_out[] = { TC_CODEC_MPEG1, TC_CODEC_MPEG2, TC_CODEC_XVID, TC_CODEC_ERROR };
+static const int fake_mpeg_codecs_out[] = { TC_CODEC_MPEG1VIDEO, TC_CODEC_MPEG2VIDEO, TC_CODEC_XVID, TC_CODEC_ERROR };
 static TCModuleInfo fake_mpeg_enc = {
     TC_MODULE_FEATURE_ENCODE | TC_MODULE_FEATURE_VIDEO,
     TC_MODULE_FLAG_NONE,
@@ -75,7 +75,7 @@ static TCModuleInfo fake_vorbis_enc = {
     fake_vorbis_codecs_out
 };
 
-static const int fake_avi_codecs_in[] = { TC_CODEC_MPEG1, TC_CODEC_XVID, TC_CODEC_MP3, TC_CODEC_ERROR };
+static const int fake_avi_codecs_in[] = { TC_CODEC_MPEG1VIDEO, TC_CODEC_XVID, TC_CODEC_MP3, TC_CODEC_ERROR };
 static TCModuleInfo fake_avi_mplex = {
     TC_MODULE_FEATURE_MULTIPLEX | TC_MODULE_FEATURE_VIDEO
         | TC_MODULE_FEATURE_AUDIO,
@@ -108,48 +108,74 @@ void test_module_log(void)
     }
 }
 
-static void test_match_helper(TCModuleInfo *m1, TCModuleInfo *m2, int expected)
+static int test_match_helper(int codec,
+                              TCModuleInfo *m1,
+                              TCModuleInfo *m2,
+                              int expected)
 {
-    int match = tc_module_info_match(m1, m2);
+    int match = tc_module_info_match(codec, m1, m2);
+    const char *str = tc_codec_to_string(codec);
+    int err = 0;
 
+#ifdef VERBOSE    
+    tc_log_msg(__FILE__, "codec: %s (0x%x)", str, codec);
+#endif
     if (match != expected) {
         tc_log_error(__FILE__, "'%s' <-%c-> '%s' FAILED",
                         m1->name,
                         (expected == 1) ?'-' :'!',
                         m2->name);
+        err = 1;
     } else {
         tc_log_info(__FILE__, "'%s' <-%c-> '%s' OK",
                         m1->name,
                         (expected == 1) ?'-' :'!',
                         m2->name);
     }
-
+    return err;
 }
 
-void test_module_match(void)
+int test_module_match(void)
 {
-    test_match_helper(&empty, &empty, 0);
-    test_match_helper(&empty, &fake_mpeg_enc, 0);
-    test_match_helper(&fake_mpeg_enc, &empty, 0);
+    int errors = 0;
 
-    test_match_helper(&pass_enc, &fake_mplex, 1);
-    test_match_helper(&pass_enc, &fake_avi_mplex, 0);
-    test_match_helper(&pass_enc, &fake_mpeg_enc, 0);
+    errors += test_match_helper(TC_CODEC_ANY, &empty, &empty, 0);
+    errors += test_match_helper(TC_CODEC_ANY, &empty, &fake_mpeg_enc, 0);
+    errors += test_match_helper(TC_CODEC_ANY, &fake_mpeg_enc, &empty, 0);
 
-    test_match_helper(&fake_mpeg_enc, &fake_vorbis_enc, 0);
-    test_match_helper(&fake_mpeg_enc, &fake_mplex, 1);
-    test_match_helper(&fake_mpeg_enc, &fake_avi_mplex, 1);
+    errors += test_match_helper(TC_CODEC_ANY, &pass_enc, &fake_mplex, 1);
+    errors += test_match_helper(TC_CODEC_ANY, &pass_enc, &fake_avi_mplex, 1);
+ 
+//  this is tricky. Should fail since there are two *encoders* chained
+//  and this make no sense *in our current architecture*.
+//  but from tcmoduleinfo infrastructure POV, it make perfectly sense (yet)
+//  since encoders involved have compatible I/O capabilties, so it doesn't fail.
+//    errors += test_match_helper(TC_CODEC_ANY, &pass_enc, &fake_mpeg_enc, 0);
 
-    test_match_helper(&fake_vorbis_enc, &fake_mpeg_enc, 0);
-    test_match_helper(&fake_vorbis_enc, &fake_mplex, 1);
-    test_match_helper(&fake_vorbis_enc, &fake_avi_mplex, 0);
+    errors += test_match_helper(TC_CODEC_MPEG2VIDEO, &fake_mpeg_enc, &fake_vorbis_enc, 0);
+    errors += test_match_helper(TC_CODEC_ANY, &fake_mpeg_enc, &fake_mplex, 1);
+    errors += test_match_helper(TC_CODEC_MPEG1VIDEO, &fake_mpeg_enc, &fake_mplex, 1);
+    errors += test_match_helper(TC_CODEC_ANY, &fake_mpeg_enc, &fake_avi_mplex, 1);
+    errors += test_match_helper(TC_CODEC_MPEG1VIDEO, &fake_mpeg_enc, &fake_avi_mplex, 1);
+    errors += test_match_helper(TC_CODEC_XVID, &fake_mpeg_enc, &fake_avi_mplex, 1);
+
+    errors += test_match_helper(TC_CODEC_VORBIS, &fake_vorbis_enc, &fake_mpeg_enc, 0);
+    errors += test_match_helper(TC_CODEC_VORBIS, &fake_vorbis_enc, &fake_mplex, 1);
+    errors += test_match_helper(TC_CODEC_VORBIS, &fake_vorbis_enc, &fake_avi_mplex, 0);
+
+    return errors;
 }
 
 int main(void)
 {
-    test_module_match();
+    int errors = test_module_match();
 
-    return 0;
+    putchar('\n');
+    tc_log_info(__FILE__, "test summary: %i error%s (%s)",
+                errors,
+                (errors > 1) ?"s" :"",
+                (errors > 0) ?"FAILED" :"PASSED");
+    return (errors > 0) ?1 :0;
 }
 
 /*************************************************************************/
