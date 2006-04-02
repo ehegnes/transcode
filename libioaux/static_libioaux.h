e514c7e1dd51114e0415528cf49983834920bb8a
Francesco Romani <fromani@gmail.com>
2006-04-02 11:43:20 +0000

    add static linkage header helper for libioaux.

---
+++ libioaux/static_libioaux.h
@@ -0,0 +1,11 @@
+
+#ifndef _STATIC_LIBIOAUX_H_
+#define _STATIC_LIBIOAUX_H_
+
+#include "libioaux/configs.h"
+void dummy_libioaux(void);
+void dummy_libioaux(void) {
+    module_print_config(NULL, NULL);		
+}
+
+#endif /* _STATIC_LIBIOAUX_H_ */
