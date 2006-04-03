7ff88a781bcf183acdbb0cde3739adcd6074813a
Francesco Romani <fromani@gmail.com>
2006-04-03 19:22:28 +0000

    little spacing cosmetic fix.

--- libtc/libtc.h
+++ libtc/libtc.h
@@ -32,11 +32,12 @@
 #include <stdarg.h>
 
 #ifdef HAVE_STDINT_H
-#include <stdint.h>
+# include <stdint.h>
 #endif
 #ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
+# include <sys/types.h>
 #endif
+
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
