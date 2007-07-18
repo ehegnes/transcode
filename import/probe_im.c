bac512e1600af3c0ea0d0a281d2f63db5a8fde7d
Francesco Romani <fromani@gmail.com>
2007-07-18 17:41:01 +0000

    backport new IM modules from CVS HEAD.

--- import/probe_im.c
+++ import/probe_im.c
@@ -1,71 +1,100 @@
 /*
  *  probe_im.c
  *
- *  Copyright (C) Thomas Östreich - June 2001
+ *  Copyright (C) Thomas Oestreich - June 2001
  *
  *  This file is part of transcode, a video stream processing tool
- *      
+ *
  *  transcode is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
  *  any later version.
- *   
+ *
  *  transcode is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *   
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
 
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef HAVE_IMAGEMAGICK
+/* Note: because of ImageMagick bogosity, this must be included first, so
+ * we can undefine the PACKAGE_* symbols it splats into our namespace.
+ * However, since we need config.h to find out whether we can include it
+ * in the first place, we also have to undefine the symbols beforehand. */
+# undef PACKAGE_BUGREPORT
+# undef PACKAGE_NAME
+# undef PACKAGE_STRING
+# undef PACKAGE_TARNAME
+# undef PACKAGE_VERSION
+# include <wand/MagicWand.h>
+# undef PACKAGE_BUGREPORT
+# undef PACKAGE_NAME
+# undef PACKAGE_STRING
+# undef PACKAGE_TARNAME
+# undef PACKAGE_VERSION
+#endif
+
+#include "transcode.h"
+#include "tcinfo.h"
 #include "ioaux.h"
 #include "tc.h"
+#include "libtc/libtc.h"
 
 #ifdef HAVE_IMAGEMAGICK
 
-// transcode defines this as well as ImageMagick.
-#undef PACKAGE_NAME
-#undef PACKAGE_TARNAME
-#undef PACKAGE_VERSION
-#undef PACKAGE_STRING
-
-#include <magick/api.h>
 
 void probe_im(info_t *ipipe)
 {
-	ExceptionInfo exception_info;
-	Image* image;
-	ImageInfo* image_info;
+    MagicWand *wand = NULL;
+    MagickBooleanType status;
 
-	InitializeMagick("");
+    MagickWandGenesis();
+    wand = NewMagickWand();
 
-	GetExceptionInfo(&exception_info);
-	image_info = CloneImageInfo((ImageInfo*)NULL);
-	strlcpy(image_info->filename, ipipe->name, MaxTextExtent);
-	image = ReadImage(image_info, &exception_info);
-	if (image == NULL)
-	{
-		MagickError(exception_info.severity, exception_info.reason, exception_info.description);
-		DestroyImageInfo(image_info);
-		ipipe->error=1;
-		return;
-	}
-  
+    if (wand == NULL) {
+        tc_log_error(__FILE__, "cannot create magick wand");
+        ipipe->error = 1;
+        return;
+    }
+
+    status = MagickReadImage(wand, ipipe->name);
+    if (status == MagickFalse) {
+        ExceptionType severity;
+        const char *description = MagickGetException(wand, &severity);
+
+        tc_log_error(__FILE__, "%s", description);
+
+        MagickRelinquishMemory((void*)description);
+        ipipe->error = 1;
+        return;
+    }
+    MagickSetLastIterator(wand);
+
+
 	/* read all video parameter from input file */
-	ipipe->probe_info->width = image->columns;
-	ipipe->probe_info->height = image->rows;
+	ipipe->probe_info->width = MagickGetImageWidth(wand);
+	ipipe->probe_info->height = MagickGetImageHeight(wand);
 
 	/* slide show? */
 	ipipe->probe_info->fps = 1;
-  
+
 	ipipe->probe_info->magic = ipipe->magic;
 	ipipe->probe_info->codec = TC_CODEC_RGB;
 
-	DestroyImage(image);
-	DestroyImageInfo(image_info);
+    DestroyMagicWand(wand);
+    MagickWandTerminus();
 
 	return;
 }
@@ -74,10 +103,21 @@ void probe_im(info_t *ipipe)
 
 void probe_im(info_t *ipipe)
 {
-	fprintf(stderr, "(%s) no support for ImageMagick compiled - exit.\n", __FILE__);
-	ipipe->error=1;
+	tc_log_error(__FILE__, "no support for ImageMagick compiled - exit.");
+	ipipe->error = 1;
 	return;
 }
 
 #endif
 
+/*************************************************************************/
+
+/*
+ * Local variables:
+ *   c-file-style: "stroustrup"
+ *   c-file-offsets: ((case-label . *) (statement-case-intro . *))
+ *   indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+ */
