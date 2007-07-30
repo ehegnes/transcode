4597bafc552d3d691c06394f74d2e643ed068584
Francesco Romani <fromani@gmail.com>
2007-07-30 20:51:13 +0000

    don't commit (again, groan) work in progress...

--- filter/filter_compare.c
+++ filter/filter_compare.c
@@ -37,7 +37,7 @@
 #include "libtc/optstr.h"
 
 #define MOD_NAME    "filter_compare.so"
-#define MOD_VERSION "v0.2.0 (2007-07-29)"
+#define MOD_VERSION "v0.1.2 (2003-08-29)"
 #define MOD_CAP     "compare with other image to find a pattern"
 #define MOD_AUTHOR  "Antonio Beamud"
 
@@ -48,35 +48,40 @@
 
 #define DELTA_COLOR 45.0
 
+// FIXME: Try to implement the YUV colorspace
 
-typedef struct PixelsMask {
-	uint32_t row;
-	uint32_t col;
-	uint8_t r, g, b;
-	uint8_t delta_r, delta_g, delta_b;
+typedef struct pixelsMask {
+	unsigned int row;
+	unsigned int col;
+	unsigned char r,g,b;
+	unsigned char delta_r,delta_g,delta_b;
 
-	struct PixelsMask *next;
-} PixelsMask;
+	struct pixelsMask *next;
 
-typedef struct ComparePrivateData {
+}pixelsMask;
+
+typedef struct compareData {
+
 	FILE *results;
 
 	float delta;
 	int step;
 
-	PixelsMask *pixel_mask;
+	pixelsMask *pixel_mask;
 
 	vob_t *vob;
 
-	uint32_t frames;
+	unsigned int frames;
 
 	int width, height;
 	int size;
-} ComparePrivateData;
+
+} compareData;
 
 /* FIXME: this uses the filter ID as an index--the ID can grow
  * arbitrarily large, so this needs to be fixed */
-static ComparePrivateData *compare[100];
+static compareData *compare[100];
+extern int rgbswap;
 
 /*-------------------------------------------------
  *
@@ -107,7 +112,7 @@ int tc_filter(frame_list_t *ptr_, char *options)
 	ImageInfo *image_info;
 
 	PixelPacket *pixel_packet;
-	PixelsMask *pixel_last;
+	pixelsMask *pixel_last;
 	ExceptionInfo exception_info;
 
 	if(ptr->tag & TC_FILTER_GET_CONFIG) {
@@ -115,11 +120,11 @@ int tc_filter(frame_list_t *ptr_, char *options)
 		optstr_filter_desc(options, MOD_NAME, MOD_CAP, MOD_VERSION,
 				   MOD_AUTHOR, "VRYMO", "1");
 
-		tc_snprintf(buf, 128, "/dev/compare");
+		tc_snprintf(buf, 128, "/dev/null");
 		optstr_param(options, "pattern", "Pattern image file path", "%s", buf);
 		tc_snprintf(buf, 128, "results.dat");
 		optstr_param(options, "results", "Results file path" , "%s", buf);
-		tc_snprintf(buf, 128, "%f", pd->delta);
+		tc_snprintf(buf, 128, "%f", compare[instance]->delta);
 		optstr_param(options, "delta", "Delta error", "%f",buf,"0.0", "100.0");
 		return 0;
 	}
@@ -135,26 +140,26 @@ int tc_filter(frame_list_t *ptr_, char *options)
 	{
 
 		unsigned int t,r,index;
-		PixelsMask *temp;
+		pixelsMask *temp;
 
-		pd = tc_malloc(sizeof(ComparePrivateData));
-		if(pd == NULL)
+		compare[instance] = tc_malloc(sizeof(compareData));
+		if(compare[instance] == NULL)
 			return (-1);
 
-		pd->vob = tc_get_vob();
-		if(pd->vob ==NULL)
+		compare[instance]->vob = tc_get_vob();
+		if(compare[instance]->vob ==NULL)
             return(-1);
 
-		pd->delta=DELTA_COLOR;
-		pd->step=1;
-		pd->width=0;
-		pd->height=0;
-		pd->frames = 0;
-		pd->pixel_mask = NULL;
+		compare[instance]->delta=DELTA_COLOR;
+		compare[instance]->step=1;
+		compare[instance]->width=0;
+		compare[instance]->height=0;
+		compare[instance]->frames = 0;
+		compare[instance]->pixel_mask = NULL;
 		pixel_last = NULL;
 
-		pd->width = pd->vob->ex_v_width;
-		pd->height = pd->vob->ex_v_height;
+		compare[instance]->width = compare[instance]->vob->ex_v_width;
+		compare[instance]->height = compare[instance]->vob->ex_v_height;
 
 		if (options != NULL) {
 			char pattern_name[PATH_MAX];
@@ -166,13 +171,13 @@ int tc_filter(frame_list_t *ptr_, char *options)
 
 			optstr_get(options, "pattern", "%[^:]", pattern_name);
 			optstr_get(options, "results", "%[^:]", results_name);
-			optstr_get(options, "delta", "%f", &pd->delta);
+			optstr_get(options, "delta", "%f", &compare[instance]->delta);
 
 			if (verbose > 1) {
 				tc_log_info(MOD_NAME, "Compare Image Settings:");
 				tc_log_info(MOD_NAME, "      pattern = %s\n", pattern_name);
 				tc_log_info(MOD_NAME, "      results = %s\n", results_name);
-				tc_log_info(MOD_NAME, "        delta = %f\n", pd->delta);
+				tc_log_info(MOD_NAME, "        delta = %f\n", compare[instance]->delta);
 			}
 
 			if (strlen(results_name) == 0) {
@@ -180,7 +185,7 @@ int tc_filter(frame_list_t *ptr_, char *options)
 				strlcpy(results_name, "/tmp/compare.dat", sizeof(results_name));
 
 			}
-			if (!(pd->results = fopen(results_name, "w")))
+			if (!(compare[instance]->results = fopen(results_name, "w")))
 			{
 				tc_log_perror(MOD_NAME, "could not open file for writing");
 			}
@@ -201,7 +206,7 @@ int tc_filter(frame_list_t *ptr_, char *options)
 				MagickWarning(exception_info.severity,
 					      exception_info.reason,
 					      exception_info.description);
-				strlcpy(pattern_name, "/dev/compare", sizeof(pattern_name));
+				strlcpy(pattern_name, "/dev/null", sizeof(pattern_name));
 			}else{
 			       if (verbose > 1)
 			       		tc_log_info(MOD_NAME, "Image loaded successfully");
@@ -218,16 +223,16 @@ int tc_filter(frame_list_t *ptr_, char *options)
 			}
 
 
-		fprintf(pd->results,"#fps:%f\n",pd->vob->fps);
+		fprintf(compare[instance]->results,"#fps:%f\n",compare[instance]->vob->fps);
 
 		if (orig != NULL){
                         // Flip and resize
-			if (pd->vob->im_v_codec == CODEC_YUV)
+			if (compare[instance]->vob->im_v_codec == CODEC_YUV)
 				TransformRGBImage(orig,YCbCrColorspace);
 			if (verbose > 1) tc_log_info(MOD_NAME, "Resizing the Image");
 			resized = ResizeImage(orig,
-					      pd->width,
-					      pd->height,
+					      compare[instance]->width,
+					      compare[instance]->height,
 					      GaussianFilter,
 					      1,
 					      &exception_info);
@@ -253,7 +258,7 @@ int tc_filter(frame_list_t *ptr_, char *options)
 				for (r = 0; r < pattern->columns; r++){
 					index = t*pattern->columns + r;
 					if (pixel_packet[index].opacity == 0){
-						temp=tc_malloc(sizeof(struct PixelsMask));
+						temp=tc_malloc(sizeof(struct pixelsMask));
 						temp->row=t;
 						temp->col=r;
 						temp->r = (uint8_t)ScaleQuantumToChar(pixel_packet[index].red);
@@ -263,7 +268,7 @@ int tc_filter(frame_list_t *ptr_, char *options)
 
 						if (pixel_last == NULL){
 							pixel_last = temp;
-							pd->pixel_mask = temp;
+							compare[instance]->pixel_mask = temp;
 						}else{
 							pixel_last->next = temp;
 							pixel_last = temp;
@@ -288,12 +293,12 @@ int tc_filter(frame_list_t *ptr_, char *options)
 
 	if(ptr->tag & TC_FILTER_CLOSE) {
 
-		if (pd != NULL) {
-			fclose(pd->results);
-			free(pd);
+		if (compare[instance] != NULL) {
+			fclose(compare[instance]->results);
+			free(compare[instance]);
 		}
 		DestroyMagick();
-		pd=NULL;
+		compare[instance]=NULL;
 
 		return(0);
 
@@ -306,27 +311,31 @@ int tc_filter(frame_list_t *ptr_, char *options)
 	//----------------------------------
 
 
+	// tag variable indicates, if we are called before
+	// transcodes internal video/audio frame processing routines
+	// or after and determines video/audio context
+
 	if((ptr->tag & TC_POST_M_PROCESS) && (ptr->tag & TC_VIDEO))  {
 		// For now I only support RGB color space
-		PixelsMask *item = NULL;
+		pixelsMask *item = NULL;
 		double sr,sg,sb;
 		double avg_dr,avg_dg,avg_db;
 
-		if (pd->vob->im_v_codec == CODEC_RGB){
+		if (compare[instance]->vob->im_v_codec == CODEC_RGB){
 
 			int r,g,b,c;
 			double width_long;
 
-			if (pd->pixel_mask != NULL)
+			if (compare[instance]->pixel_mask != NULL)
 			{
-				item = pd->pixel_mask;
+				item = compare[instance]->pixel_mask;
 				c = 0;
 
 				sr = 0.0;
 				sg = 0.0;
 				sb = 0.0;
 
-				width_long = pd->width*3;
+				width_long = compare[instance]->width*3;
 				while(item){
 					r = item->row*width_long + item->col*3;
 					g = item->row*width_long
@@ -347,20 +356,67 @@ int tc_filter(frame_list_t *ptr_, char *options)
 				avg_dg = sg/(double)c;
 				avg_db = sb/(double)c;
 
-				if ((avg_dr < pd->delta) && (avg_dg < pd->delta) && (avg_db < pd->delta))
-					fprintf(pd->results,"1");
+				if ((avg_dr < compare[instance]->delta) && (avg_dg < compare[instance]->delta) && (avg_db < compare[instance]->delta))
+					fprintf(compare[instance]->results,"1");
 				else
-					fprintf(pd->results,"n");
-				fflush(pd->results);
+					fprintf(compare[instance]->results,"n");
+				fflush(compare[instance]->results);
 			}
-			pd->frames++;
+			compare[instance]->frames++;
 			return(0);
+		}else{
+
+                        // The colospace is YUV
+
+                        // FIXME: Doesn't works, I need to code all this part
+			// again
+
+			int Y,Cr,Cb,c;
+
+			if (compare[instance]->pixel_mask != NULL)
+			{
+				item = compare[instance]->pixel_mask;
+				c = 0;
+
+				sr = 0.0;
+				sg = 0.0;
+				sb = 0.0;
+
+				while(item){
+					Y  = item->row*compare[instance]->width + item->col;
+					Cb = compare[instance]->height*compare[instance]->width
+						+ (int)((item->row*compare[instance]->width + item->col)/4);
+					Cr = compare[instance]->height*compare[instance]->width
+						+ (int)((compare[instance]->height*compare[instance]->width)/4)
+						+ (int)((item->row*compare[instance]->width + item->col)/4);
+
+				        // diff between points
+				        // Interchange RGB values if necesary
+
+					sr = sr + (double)abs((unsigned char)ptr->video_buf[Y] - item->r);
+					sg = sg + (double)abs((unsigned char)ptr->video_buf[Cb] - item->g);
+					sb = sb + (double)abs((unsigned char)ptr->video_buf[Cr] - item->b);
+					item = item->next;
+					c++;
+				}
+
+				avg_dr = sr/(double)c;
+				avg_dg = sg/(double)c;
+				avg_db = sb/(double)c;
+
+				if ((avg_dr < compare[instance]->delta) && (avg_dg < compare[instance]->delta) && (avg_db < compare[instance]->delta))
+					fprintf(compare[instance]->results,"1");
+				else
+					fprintf(compare[instance]->results,"n");
+			}
+			compare[instance]->frames++;
+			return(0);
+
 		}
 	}
 
 	return(0);
 }
-
 // Proposal:
 // Tilmann Bitterberg Sat, 14 Jun 2003 00:29:06 +0200 (CEST)
 //
@@ -378,105 +434,10 @@ int tc_filter(frame_list_t *ptr_, char *options)
 //      pixel_packet->green == *Cr++;
 //      pixel_packet->blue == *Cb++;
 //      get_next_pixel();
+//    }
 
-/*************************************************************************/
 
-static const TCCodecID compare_codecs_in[] = { 
-    TC_CODEC_RGB, TC_CODEC_ERROR
-};
-static const TCCodecID compare_codecs_out[] = {
-    TC_CODEC_RGB, TC_CODEC_ERROR
-};
-static const TCFormatID compare_formats[] = {
-    TC_FORMAT_ERROR
-};
 
-static const TCModuleInfo compare_info = {
-    .features    = MOD_FEATURES,
-    .flags       = MOD_FLAGS,
-    .name        = MOD_NAME,
-    .version     = MOD_VERSION,
-    .description = MOD_CAP,
-    .codecs_in   = compare_codecs_in,
-    .codecs_out  = compare_codecs_out,
-    .formats_in  = compare_formats,
-    .formats_out = compare_formats
-};
 
-static const TCModuleClass compare_class = {
-    .info         = &compare_info,
 
-    .init         = compare_init,
-    .fini         = compare_fini,
-    .configure    = compare_configure,
-    .stop         = compare_stop,
-    .inspect      = compare_inspect,
 
-    .filter_video = compare_filter_video,
-};
-
-extern const TCModuleClass *tc_plugin_setup(void)
-{
-    return &compare_class;
-}
-
-/*************************************************************************/
-
-static int compare_get_config(TCModuleInstance *self, char *options)
-{
-    ComparePrivateData *pd = NULL;
-
-    TC_MODULE_SELF_CHECK(self, "get_config");
-
-    pd = self->userdata;
-
-    /*
-     * Valid flags for the string of filter capabilities:
-     *  "V" :  Can do Video
-     *  "A" :  Can do Audio
-     *  "R" :  Can do RGB
-     *  "Y" :  Can do YUV
-     *  "4" :  Can do YUV422
-     *  "M" :  Can do Multiple Instances
-     *  "E" :  Is a PRE filter
-     *  "O" :  Is a POST filter
-     */
-    optstr_filter_desc(options, MOD_NAME, MOD_CAP, MOD_VERSION,
-                       MOD_AUTHOR, "VAMEO", "1");
-
-    /* can be omitted */
-    optstr_param(options, "help", "Prints out a short help", "", "0");
- 
-    /* use optstr_param to do introspection */
-
-    return TC_OK;
-}
-
-static int compare_process(TCModuleInstance *self, frame_list_t *frame)
-{
-    TC_MODULE_SELF_CHECK(self, "process");
-
-    /* choose what to do by frame->tag */
-	if((ptr->tag & TC_POST_M_PROCESS) && (ptr->tag & TC_VIDEO))  {
-        return compare_filter_video(self, (vframe_list_t*)frame);
-    }
-    return TC_OK;
-}
-
-/*************************************************************************/
-
-/* Old-fashioned module interface. */
-
-TC_FILTER_OLDINTERFACE(compare)
-
-/*************************************************************************/
-
-/*
- * Local variables:
- *   c-file-style: "stroustrup"
- *   c-file-offsets: ((case-label . *) (statement-case-intro . *))
- *   indent-tabs-mode: nil
- * End:
- *
- * vim: expandtab shiftwidth=4:
- */
