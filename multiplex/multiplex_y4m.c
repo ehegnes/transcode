a5adfa730ac722a2c94bdb729484e67886160d76
Francesco Romani <fromani@gmail.com>
2007-11-18 10:05:42 +0000

    split old multiplex_y4m in true multiplex_y4m and separate multiplex_wav.c each of those mplexors can really produce a single stream (only video or only audio respectively).

--- multiplex/multiplex_y4m.c
+++ multiplex/multiplex_y4m.c
@@ -1,6 +1,5 @@
 /*
  *  multiplex_y4m.c -- pack a yuv420p stream in YUV4MPEG2 format
- *                     and/or a pcm stream in WAVE format
  *  (C) 2005-2007 Francesco Romani <fromani at gmail dot com>
  *
  * This file is part of transcode, a video stream processing tool.
@@ -43,11 +42,9 @@
 #include "mjpegtools/mpegconsts.h"
 #endif
 
-#include "avilib/wavlib.h"
-
 #define MOD_NAME    "multiplex_y4m.so"
-#define MOD_VERSION "v0.0.1 (2006-03-22)"
-#define MOD_CAP     "write YUV4MPEG2 video and WAVE audio streams"
+#define MOD_VERSION "v0.1.0 (2007-11-17)"
+#define MOD_CAP     "write YUV4MPEG2 video stream"
 
 #define MOD_FEATURES \
     TC_MODULE_FEATURE_MULTIPLEX|TC_MODULE_FEATURE_VIDEO|TC_MODULE_FEATURE_AUDIO
@@ -57,51 +54,37 @@
     TC_MODULE_FLAG_RECONFIGURABLE
     
 
-
-#define YW_VID_EXT "y4m"
-#define YW_AUD_EXT "wav"
-
-/* 
- * 'yw_' prefix is used internally to avoid any name clash
- * with mjpegtools's y4m_* routines
- */
-
-
-/* XXX */
-static const char yw_help[] = ""
+static const char tc_y4m_help[] = ""
     "Overview:\n"
     "    this module writes a yuv420p video stream using YUV4MPEG2 format\n"
-    "    and/or a pcm stream using WAVE format.\n"
     "Options:\n"
     "    help    produce module overview and options explanations\n";
 
 typedef struct {
     int fd_vid;
-    WAV wav;
 
     y4m_frame_info_t frameinfo;
     y4m_stream_info_t streaminfo;
 
     int width;
     int height;
-    
-} YWPrivateData;
+} Y4MPrivateData;
 
 
-static int yw_inspect(TCModuleInstance *self,
+static int tc_y4m_inspect(TCModuleInstance *self,
                       const char *options, const char **value)
 {
     TC_MODULE_SELF_CHECK(self, "inspect");
     
     if (optstr_lookup(options, "help")) {
-        *value = yw_help;
+        *value = tc_y4m_help;
     }
 
     return TC_OK;
 }
 
-static int yw_open_video(YWPrivateData *pd, const char *filename,
-                         vob_t *vob)
+static int tc_y4m_open_video(Y4MPrivateData *pd, const char *filename,
+                             vob_t *vob)
 {
     int asr, ret;
     y4m_ratio_t framerate;
@@ -148,9 +131,9 @@ static int yw_open_video(YWPrivateData *pd, const char *filename,
                                           asr_rate));
     y4m_si_set_height(&(pd->streaminfo), pd->height);
     y4m_si_set_width(&(pd->streaminfo), pd->width);
-    /* Y4M_CHROMA_420JPEG     4:2:0, H/V centered, for JPEG/MPEG-1 */
-    /* Y4M_CHROMA_420MPEG2   4:2:0, H cosited, for MPEG-2         */
-    /* Y4M_CHROMA_420PALDV   4:2:0, alternating Cb/Cr, for PAL-DV */
+    /* Y4M_CHROMA_420JPEG   4:2:0, H/V centered, for JPEG/MPEG-1 */
+    /* Y4M_CHROMA_420MPEG2  4:2:0, H cosited, for MPEG-2         */
+    /* Y4M_CHROMA_420PALDV  4:2:0, alternating Cb/Cr, for PAL-DV */
     y4m_si_set_chroma(&(pd->streaminfo), Y4M_CHROMA_420JPEG); // XXX
     
     ret = y4m_write_stream_header(pd->fd_vid, &(pd->streaminfo));
@@ -162,86 +145,32 @@ static int yw_open_video(YWPrivateData *pd, const char *filename,
     return TC_OK;
 }
 
-static int yw_open_audio(YWPrivateData *pd, const char *filename,
-                         vob_t *vob)
-{
-    WAVError err;
-    int rate;
-
-    pd->wav = wav_open(filename, WAV_WRITE, &err);
-    if (!pd->wav) {
-        tc_log_error(MOD_NAME, "failed to open audio stream file '%s'"
-                               " (reason: %s)", filename,
-                               wav_strerror(err));
-        return TC_ERROR;
-    }
-
-    rate = (vob->mp3frequency != 0) ?vob->mp3frequency :vob->a_rate;
-    wav_set_bits(pd->wav, vob->dm_bits);
-    wav_set_rate(pd->wav, rate);
-    wav_set_bitrate(pd->wav, vob->dm_chan * rate * vob->dm_bits/8);
-    wav_set_channels(pd->wav, vob->dm_chan);
-
-    return TC_OK;
-}
-
-static int yw_configure(TCModuleInstance *self,
+static int tc_y4m_configure(TCModuleInstance *self,
                          const char *options, vob_t *vob)
 {
-    char vid_name[PATH_MAX];
-    char aud_name[PATH_MAX];
-    YWPrivateData *pd = NULL;
-    int ret;
+    Y4MPrivateData *pd = NULL;
 
     TC_MODULE_SELF_CHECK(self, "configure");
 
     pd = self->userdata;
 
-    if (vob->audio_out_file == NULL
-      || !strcmp(vob->audio_out_file, "/dev/null")) {
-        /* use affine names */
-        tc_snprintf(vid_name, PATH_MAX, "%s.%s",
-                    vob->video_out_file, YW_VID_EXT);
-        tc_snprintf(aud_name, PATH_MAX, "%s.%s",
-                    vob->video_out_file, YW_AUD_EXT);
-    } else {
-        /* copy names verbatim */
-        strlcpy(vid_name, vob->video_out_file, PATH_MAX);
-        strlcpy(aud_name, vob->audio_out_file, PATH_MAX);
-    }
-    
-    pd->width = vob->ex_v_width;
+    pd->width  = vob->ex_v_width;
     pd->height = vob->ex_v_height;
     
-    ret = yw_open_video(pd, vid_name, vob);
-    if (ret != TC_OK) {
-        return ret;
-    }
-    ret = yw_open_audio(pd, aud_name, vob);
-    if (ret != TC_OK) {
-        return ret;
-    }
-    if (vob->verbose >= TC_DEBUG) {
-        tc_log_info(MOD_NAME, "video output: %s (%s)",
-                    vid_name, (pd->fd_vid == -1) ?"FAILED" :"OK");
-        tc_log_info(MOD_NAME, "audio output: %s (%s)",
-                    aud_name, (pd->wav == NULL) ?"FAILED" :"OK");
-    }
-    return TC_OK;
+    return tc_y4m_open_video(pd, vob->video_out_file, vob);
 }
 
-static int yw_stop(TCModuleInstance *self)
+static int tc_y4m_stop(TCModuleInstance *self)
 {
-    YWPrivateData *pd = NULL;
-    int verr, aerr;
+    Y4MPrivateData *pd = NULL;
 
     TC_MODULE_SELF_CHECK(self, "stop");
 
     pd = self->userdata;
 
     if (pd->fd_vid != -1) {
-        verr = close(pd->fd_vid);
-        if (verr) {
+        int err = close(pd->fd_vid);
+        if (err) {
             tc_log_error(MOD_NAME, "closing video file: %s",
                                    strerror(errno));
             return TC_ERROR;
@@ -252,33 +181,23 @@ static int yw_stop(TCModuleInstance *self)
         pd->fd_vid = -1;
     }
 
-    if (pd->wav != NULL) {
-        aerr = wav_close(pd->wav);
-        if (aerr != 0) {
-            tc_log_error(MOD_NAME, "closing audio file: %s",
-                                   wav_strerror(wav_last_error(pd->wav)));
-            return TC_ERROR;
-        }
-        pd->wav = NULL;
-    }
-
     return TC_OK;
 }
 
-static int yw_multiplex(TCModuleInstance *self,
-                         vframe_list_t *vframe, aframe_list_t *aframe)
+static int tc_y4m_multiplex(TCModuleInstance *self,
+                            vframe_list_t *vframe, aframe_list_t *aframe)
 {
-    ssize_t w_aud = 0, w_vid = 0;
+    ssize_t w_vid = 0;
 
-    YWPrivateData *pd = NULL;
+    Y4MPrivateData *pd = NULL;
 
     TC_MODULE_SELF_CHECK(self, "multiplex");
 
     pd = self->userdata;
-
     if (vframe != NULL && vframe->video_len > 0) {
-        uint8_t *planes[3];
+        uint8_t *planes[3] = { NULL, NULL, NULL };
         int ret = 0;
+
         y4m_init_frame_info(&pd->frameinfo);
         YUV_INIT_PLANES(planes, vframe->video_buf, IMG_YUV420P,
                         pd->width, pd->height);
@@ -294,33 +213,28 @@ static int yw_multiplex(TCModuleInstance *self,
     }
 
     if (aframe != NULL && aframe->audio_len > 0) {
-        w_aud = wav_write_data(pd->wav, aframe->audio_buf, aframe->audio_len);
-        if (w_aud != aframe->audio_len) {
-            tc_log_warn(MOD_NAME, "error while writing audio frame: %s",
-                                  wav_strerror(wav_last_error(pd->wav)));
-            return TC_ERROR;
-        }
+        return TC_OK;
     }
 
-    return (int)(w_vid + w_aud);
+    return (int)w_vid;
 }
 
-static int yw_init(TCModuleInstance *self, uint32_t features)
+static int tc_y4m_init(TCModuleInstance *self, uint32_t features)
 {
-    YWPrivateData *pd = NULL;
+    Y4MPrivateData *pd = NULL;
 
     TC_MODULE_SELF_CHECK(self, "init");
     TC_MODULE_INIT_CHECK(self, MOD_FEATURES, features);
 
-    pd = tc_malloc(sizeof(YWPrivateData));
+    pd = tc_malloc(sizeof(Y4MPrivateData));
     if (pd == NULL) {
         return TC_ERROR;
     }
 
-    pd->width = 0;
+    pd->width  = 0;
     pd->height = 0;
     pd->fd_vid = -1;
-    pd->wav = NULL;
+
     y4m_init_stream_info(&(pd->streaminfo));
     /* frameinfo will be initialized at each multiplex call  */
 
@@ -332,11 +246,11 @@ static int yw_init(TCModuleInstance *self, uint32_t features)
     return TC_OK;
 }
 
-static int yw_fini(TCModuleInstance *self)
+static int tc_y4m_fini(TCModuleInstance *self)
 {
     TC_MODULE_SELF_CHECK(self, "fini");
 
-    yw_stop(self);
+    tc_y4m_stop(self);
 
     tc_free(self->userdata);
     self->userdata = NULL;
@@ -347,43 +261,44 @@ static int yw_fini(TCModuleInstance *self)
 
 /*************************************************************************/
 
-static const TCCodecID yw_codecs_in[] = { TC_CODEC_YUV420P, TC_CODEC_PCM,
-                                     TC_CODEC_ERROR };
-
-/* a multiplexor is at the end of pipeline */
-static const TCCodecID yw_codecs_out[] = { TC_CODEC_ERROR };
-static const TCFormatID yw_formats_in[] = { TC_FORMAT_ERROR };
-static const TCFormatID yw_formats_out[] = { 
-    TC_FORMAT_YUV4MPEG, TC_FORMAT_WAV, TC_FORMAT_ERROR
+static const TCCodecID tc_y4m_codecs_in[] = { 
+    TC_CODEC_YUV420P, TC_CODEC_ERROR
 };
 
-static const TCModuleInfo yw_info = {
+/* a multiplexor is at the end of pipeline */
+static const TCCodecID tc_y4m_codecs_out[] = { TC_CODEC_ERROR };
+static const TCFormatID tc_y4m_formats_in[] = { TC_FORMAT_ERROR };
+static const TCFormatID tc_y4m_formats_out[] = { 
+    TC_FORMAT_YUV4MPEG, TC_FORMAT_ERROR
+};
+
+static const TCModuleInfo tc_y4m_info = {
     .features    = MOD_FEATURES,
     .flags       = MOD_FLAGS,
     .name        = MOD_NAME,
     .version     = MOD_VERSION,
     .description = MOD_CAP,
-    .codecs_in   = yw_codecs_in,
-    .codecs_out  = yw_codecs_out,
-    .formats_in  = yw_formats_in,
-    .formats_out = yw_formats_out
+    .codecs_in   = tc_y4m_codecs_in,
+    .codecs_out  = tc_y4m_codecs_out,
+    .formats_in  = tc_y4m_formats_in,
+    .formats_out = tc_y4m_formats_out
 };
 
-static const TCModuleClass yw_class = {
-    .info         = &yw_info,
+static const TCModuleClass tc_y4m_class = {
+    .info         = &tc_y4m_info,
 
-    .init         = yw_init,
-    .fini         = yw_fini,
-    .configure    = yw_configure,
-    .stop         = yw_stop,
-    .inspect      = yw_inspect,
+    .init         = tc_y4m_init,
+    .fini         = tc_y4m_fini,
+    .configure    = tc_y4m_configure,
+    .stop         = tc_y4m_stop,
+    .inspect      = tc_y4m_inspect,
 
-    .multiplex    = yw_multiplex,
+    .multiplex    = tc_y4m_multiplex,
 };
 
 extern const TCModuleClass *tc_plugin_setup(void)
 {
-    return &yw_class;
+    return &tc_y4m_class;
 }
 
 /*************************************************************************/
