ac19c6f080e485f16b0d9b74f0c3a74d902abfd9
Francesco Romani <fromani@gmail.com>
2008-02-22 22:04:35 +0000

    remove hardcoded PAL frame size and use maximum reported width x height instead. This should'nt hurt at all tv tuners, and should improve detection with tv cards/other v4l devices.

--- import/probe_v4l.c
+++ import/probe_v4l.c
@@ -43,99 +43,99 @@
 
 void probe_v4l(info_t *ipipe)
 {
+    int is_v4l2 = 0;
+    struct v4l2_capability caps; // v4l2
 
-  int is_v4l2 = 0, is_pal = 0;
-  struct v4l2_capability caps; // v4l2
-  struct video_capability  capability; // v4l1
+    /* yes, this is a bit sick */
+    close(ipipe->fd_in);
+    ipipe->fd_in = open(ipipe->name, O_RDWR, 0);
+    if (ipipe->fd_in < 0) {
+        tc_log_error(__FILE__, "cannot (reopen) device in RW mode: %s",
+                     strerror(errno));
+        goto error;
+    }
 
-  close(ipipe->fd_in);
-  ipipe->fd_in = open(ipipe->name, O_RDWR, 0);
-  if (ipipe->fd_in < 0) {
-      tc_log_error(__FILE__, "cannot (reopen) device in RW mode: %s",
-                   strerror(errno));
-      goto error;
-  }
+    /* let's start with good old defaults */
+    ipipe->probe_info->width  = PAL_W;
+    ipipe->probe_info->height = PAL_H;
+    ipipe->probe_info->fps    = 25;
+    ipipe->probe_info->frc    = 3;
 
-  // try a v4l2 ioctl
-  if (ipipe->verbose & TC_DEBUG)
-      tc_log_msg(__FILE__, "Checking if v4l2 ioctls are supported...");
-  if(ioctl(ipipe->fd_in, VIDIOC_QUERYCAP, &caps) < 0) {
-      is_v4l2 = 0;
-      if (ipipe->verbose & TC_DEBUG)
-          tc_log_msg(__FILE__, "... no");
-  } else {
-      is_v4l2 = 1;
-      ipipe->probe_info->magic=TC_MAGIC_V4L2_VIDEO;
-      if (ipipe->verbose & TC_DEBUG)
-          tc_log_msg(__FILE__, "... yes");
-  }
+    /* try a v4l2 ioctl */
+    if (ipipe->verbose & TC_DEBUG)
+        tc_log_msg(__FILE__, "Checking if v4l2 ioctls are supported...");
+    if (ioctl(ipipe->fd_in, VIDIOC_QUERYCAP, &caps) < 0) {
+        is_v4l2 = 0;
+        if (ipipe->verbose & TC_DEBUG)
+            tc_log_msg(__FILE__, "... no");
+    } else {
+        is_v4l2 = 1;
+        ipipe->probe_info->magic=TC_MAGIC_V4L2_VIDEO;
+        if (ipipe->verbose & TC_DEBUG)
+            tc_log_msg(__FILE__, "... yes");
+    }
+    
+    /* try v4l first */
+    if (!is_v4l2) {
+        struct video_capability  capability; // v4l1
+        if (ipipe->verbose & TC_DEBUG)
+            tc_log_msg(__FILE__, "Checking if v4l1 ioctls are supported...");
+        if (-1 == ioctl(ipipe->fd_in,VIDIOCGCAP,&capability)) {
+	        if (ipipe->verbose & TC_DEBUG)
+	            tc_log_msg(__FILE__, "... no");
+            goto error;
+        } else {
+	        ipipe->probe_info->magic = TC_MAGIC_V4L_VIDEO;
+            if (ipipe->verbose & TC_DEBUG)
+	            tc_log_msg(__FILE__, "... yes");
+        }
+        
+        ipipe->probe_info->width  = capability.maxwidth;
+        ipipe->probe_info->height = capability.maxheight;
+        /* 
+         * saa7134 sometimes delivers strange (for me) results.
+         * In the case, PAL settings are forced until I figure out
+         * if such results are correct or not.
+         */
+        if (ipipe->probe_info->width == 720 && ipipe->probe_info->height == 578) {
+            ipipe->probe_info->height = 576;
+        }
+    }
 
-  // try v4l1
-  if (!is_v4l2) {
-      if (ipipe->verbose & TC_DEBUG)
-          tc_log_msg(__FILE__, "Checking if v4l1 ioctls are supported...");
-      if (-1 == ioctl(ipipe->fd_in,VIDIOCGCAP,&capability)) {
-	  if (ipipe->verbose & TC_DEBUG)
-	      tc_log_msg(__FILE__, "... no");
-	  goto error;
-      } else {
-	  ipipe->probe_info->magic=TC_MAGIC_V4L_VIDEO;
-	  if (ipipe->verbose & TC_DEBUG)
-	      tc_log_msg(__FILE__, "... yes");
-      }
-  }
+    if (is_v4l2) {
+        v4l2_std_id std;
 
-  // I don't care much for v4l1
-  ipipe->probe_info->width  = 720;
-  ipipe->probe_info->height = 576;
-  ipipe->probe_info->fps = 25;
-  ipipe->probe_info->frc = 3;
+        if (ioctl(ipipe->fd_in, VIDIOC_G_STD, &std) >= 0) {
+            if (std & V4L2_STD_525_60) {
+                ipipe->probe_info->fps = (30000/1001);
+                ipipe->probe_info->frc = 4;
+                ipipe->probe_info->width  = 640;
+                ipipe->probe_info->height = 480;
+            } else if(std & V4L2_STD_625_50) {
+                ipipe->probe_info->fps = 25;
+                ipipe->probe_info->frc = 3;
+                ipipe->probe_info->width  = 720;
+                ipipe->probe_info->height = 576;
+	        }
+        }
+    }
 
-  if (is_v4l2) {
-
-       v4l2_std_id std;
-
-      if(ioctl(ipipe->fd_in, VIDIOC_G_STD, &std) < 0) {
-	  is_pal = 1;
-      } else {
-
-	  if (std & V4L2_STD_525_60) {
-
-	      ipipe->probe_info->fps = (30000/1001);
-	      ipipe->probe_info->frc = 4;
-	      ipipe->probe_info->width  = 640;
-	      ipipe->probe_info->height = 480;
-	      is_pal = 0;
-
-	  } else if(std & V4L2_STD_625_50) {
-
-	      ipipe->probe_info->fps = 25;
-	      ipipe->probe_info->frc = 3;
-	      ipipe->probe_info->width  = 720;
-	      ipipe->probe_info->height = 576;
-	      is_pal = 1;
-	  }
-
-      }
-  }
-
-  // FIXME: Check if these settings are actually supported by /dev/dsp
-  ipipe->probe_info->track[0].samplerate = 44100;
-  ipipe->probe_info->track[0].chan = 2;
-  ipipe->probe_info->track[0].bits = 16;
-  ipipe->probe_info->track[0].format = 0x1;
-
-  if(ipipe->probe_info->track[0].chan>0) ipipe->probe_info->num_tracks=1;
+    // FIXME: Check if these settings are actually supported by /dev/dsp
+    ipipe->probe_info->track[0].samplerate = 44100;
+    ipipe->probe_info->track[0].chan = 2;
+    ipipe->probe_info->track[0].bits = 16;
+    ipipe->probe_info->track[0].format = 0x1;
+    if (ipipe->probe_info->track[0].chan > 0)
+        ipipe->probe_info->num_tracks=1;
 
   return;
 
 error:
-  ipipe->error = 1;
-  ipipe->probe_info->codec=TC_CODEC_UNKNOWN;
-  ipipe->probe_info->magic=TC_MAGIC_UNKNOWN;
+    ipipe->error = 1;
+    ipipe->probe_info->codec=TC_CODEC_UNKNOWN;
+    ipipe->probe_info->magic=TC_MAGIC_UNKNOWN;
 
-  return;
-
+     return;
 }
 
 #else // HAVE_V4L
