8486634663b5d80aa5cf22034f43c09d3f3d7168
Francesco Romani <fromani@gmail.com>
2008-07-06 09:52:02 +0000

    drop LZO1 support, and switch to LZO2 only (mostly backport from 1.1.x).

--- export/export_lzo.c
+++ export/export_lzo.c
@@ -1,49 +1,39 @@
 /*
  *  export_lzo.c
  *
- *  Copyright (C) Thomas Östreich - October 2002
+ *  Copyright (C) Thomas Oestreich - October 2002
  *
  *  This file is part of transcode, a video stream processing tool
- *      
+ *
  *  transcode is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
  *  any later version.
- *   
+ *
  *  transcode is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *   
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#define MOD_NAME    "export_lzo.so"
+#define MOD_VERSION "v0.1.0 (2005-10-15)"
+#define MOD_CODEC   "(video) LZO real-time compression | (audio) MPEG/AC3/PCM"
 
 #include "transcode.h"
-#include "avilib.h"
+#include "libtc/libtc.h"
+#include "avilib/avilib.h"
 #include "aud_aux.h"
 #include "import/magic.h"
+#include "libtc/tc_lzo.h"
 
-#include <lzo1x.h>
-#if (LZO_VERSION > 0x1070)
-#  include <lzoutil.h>
-#endif
-
-#define LZO2 1
-#undef LZO2
-
-#ifdef LZO2
-#include "tc_lzo.h"
-#endif
-
-#define MOD_NAME    "export_lzo.so"
-#define MOD_VERSION "v0.0.6 (2003-07-24)"
-#define MOD_CODEC   "(video) LZO real-time compression | (audio) MPEG/AC3/PCM"
+#include <stdio.h>
+#include <stdlib.h>
 
 static int verbose_flag=TC_QUIET;
 static int capability_flag=TC_CAP_DV|TC_CAP_PCM|TC_CAP_RGB|TC_CAP_YUV|TC_CAP_AC3|TC_CAP_AUD|TC_CAP_VID;
@@ -63,7 +53,7 @@ static lzo_byte *wrkmem;
 static lzo_uint out_len;
 static int codec;
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * init codec
  *
@@ -71,39 +61,42 @@ static int codec;
 
 MOD_init
 {
-    
+
     if(param->flag == TC_VIDEO) {
-      if(verbose & TC_DEBUG) printf("[%s] max AVI-file size limit = %lu bytes\n", MOD_NAME, (unsigned long) AVI_max_size());
+      if(verbose & TC_DEBUG)
+          fprintf(stderr, "[%s] max AVI-file size limit = %lu bytes\n",
+                  MOD_NAME, (unsigned long) AVI_max_size());
 
       /*
        * Step 1: initialize the LZO library
        */
 
       if (lzo_init() != LZO_E_OK) {
-	printf("[%s] lzo_init() failed\n", MOD_NAME);
-	return(TC_EXPORT_ERROR); 
+          fprintf(stderr, "[%s] lzo_init() failed\n", MOD_NAME);
+          return(TC_EXPORT_ERROR); 
       }
 
       wrkmem = (lzo_bytep) lzo_malloc(LZO1X_1_MEM_COMPRESS);
       out = (lzo_bytep) lzo_malloc(vob->ex_v_height*vob->ex_v_width*3*2);
 
       if (wrkmem == NULL || out == NULL) {
-	printf("[%s] out of memory\n", MOD_NAME);
-	return(TC_EXPORT_ERROR); 
+          fprintf(stderr, "[%s] out of memory\n", MOD_NAME);
+          return(TC_EXPORT_ERROR); 
       }
 
       codec = vob->im_v_codec;
-      
+
       return(0);
     }
-    
-    if(param->flag == TC_AUDIO) return(audio_init(vob, verbose_flag));
+
+    if(param->flag == TC_AUDIO)
+       return(audio_init(vob, verbose_flag));
 
     // invalid flag
-    return(TC_EXPORT_ERROR); 
+    return(TC_EXPORT_ERROR)
 }
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * open outputfile
  *
@@ -111,52 +104,44 @@ MOD_init
 
 MOD_open
 {
-  
+
     // open out file
-    if(vob->avifile_out==NULL) 
+    if(vob->avifile_out==NULL)
       if(NULL == (vob->avifile_out = AVI_open_output_file(vob->video_out_file))) {
 	AVI_print_error("avi open error");
 	exit(TC_EXPORT_ERROR);
       }
-    
+
     /* save locally */
     avifile2 = vob->avifile_out;
-    
+
     if(param->flag == TC_VIDEO) {
-      
+
       //video
-      
+
       //force keyframe
       force_kf=1;
-      
-#ifdef LZO2
+
       AVI_set_video(vob->avifile_out, vob->ex_v_width, vob->ex_v_height, vob->ex_fps, "LZO2");
-#else
-      AVI_set_video(vob->avifile_out, vob->ex_v_width, vob->ex_v_height, vob->ex_fps, "LZO1");
-#endif
 
       if (vob->avi_comment_fd>0)
 	  AVI_set_comment_fd(vob->avifile_out, vob->avi_comment_fd);
-      
-      if(!info_shown && verbose_flag) 
-#ifdef LZO2
-	fprintf(stderr, "[%s] codec=%s, fps=%6.3f, width=%d, height=%d\n", 
-		MOD_NAME, "LZO2", vob->ex_fps, vob->ex_v_width, vob->ex_v_height);
-#else
-	fprintf(stderr, "[%s] codec=%s, fps=%6.3f, width=%d, height=%d\n", 
-		MOD_NAME, "LZO1", vob->ex_fps, vob->ex_v_width, vob->ex_v_height);
-#endif
-      
+
+      if(!info_shown && verbose_flag)
+	      fprintf(stderr, "[%s] codec=%s, fps=%6.3f, width=%d, height=%d\n", 
+		          MOD_NAME, "LZO2", vob->ex_fps, vob->ex_v_width, vob->ex_v_height);
+ 
       info_shown=1;
-      
+
       return(0);
     }
 
-    if(param->flag == TC_AUDIO) return(audio_open(vob, vob->avifile_out));
+    if(param->flag == TC_AUDIO)
+        return(audio_open(vob, vob->avifile_out));
     
     // invalid flag
-    return(TC_EXPORT_ERROR); 
-}   
+    return(TC_EXPORT_ERROR);
+}
 
 inline static void long2str(long a, unsigned char *b)
 {
@@ -172,7 +157,7 @@ inline static void short2str(short a, unsigned char *b)
       b[1] = (a&0x00ff);
 }
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * encode and export
  *
@@ -183,12 +168,10 @@ MOD_encode
 
   int key;
 
-#ifdef LZO2
   tc_lzo_header_t h;
-#endif
-  
-  if(param->flag == TC_VIDEO) { 
-    
+
+  if(param->flag == TC_VIDEO) {
+
     //write video
 
     //encode
@@ -197,7 +180,6 @@ MOD_encode
      * compress from `in' to `out' with LZO1X-1
      */
 
-#ifdef LZO2
     r = lzo1x_1_compress(param->buffer, param->size, out+sizeof(h), &out_len, wrkmem);
     h.magic = TC_CODEC_LZO2;
     h.size = out_len;
@@ -205,90 +187,86 @@ MOD_encode
     h.level = 1;
     h.flags = 0;
     h.flags |= ((codec==CODEC_RGB)?TC_LZO_FORMAT_RGB24:TC_LZO_FORMAT_YV12);
+    h.pad = 0;
 
-    // XXX
     tc_memcpy (out, &h, sizeof(h));
-#else
-    r = lzo1x_1_compress(param->buffer, param->size, out, &out_len, wrkmem);
-#endif
-    
+
     if (r == LZO_E_OK) {
-      if(verbose & TC_DEBUG) printf("compressed %lu bytes into %lu bytes\n",
+      if(verbose & TC_DEBUG) fprintf(stderr, "compressed %lu bytes into %lu bytes\n",
 				    (long) param->size, (long) out_len);
     } else {
-      
       /* this should NEVER happen */
-      printf("[%s] internal error - compression failed: %d\n", MOD_NAME, r);
+      fprintf(stderr, "[%s] internal error - compression failed: %d\n", MOD_NAME, r);
       return(TC_EXPORT_ERROR); 
     }
-    
+
     /* check for an incompressible block */
     if (out_len >= param->size)  {
-      if(verbose & TC_DEBUG) printf("[%s] block contains incompressible data\n", MOD_NAME);
-#ifdef LZO2
+      if(verbose & TC_DEBUG)
+          fprintf(stderr, "[%s] block contains incompressible data\n", MOD_NAME);
       h.flags |= TC_LZO_NOT_COMPRESSIBLE;
-#endif
+      tc_memcpy(out+sizeof(h), param->buffer, param->size);
+      out_len = param->size;
     }
-    
+
     //0.5.0-pre8:
     key = ((param->attributes & TC_FRAME_IS_KEYFRAME) || force_kf) ? 1:0;
 
-#ifdef LZO2
     out_len += sizeof(h);
-#endif
 
     //0.6.2: switch outfile on "C" and -J pv
     //0.6.2: enforce auto-split at 2G (or user value) for normal AVI files
     if((uint32_t)(AVI_bytes_written(avifile2)+out_len+16+8)>>20 >= tc_avi_limit) tc_outstream_rotate_request();
-    
+
     if(key) tc_outstream_rotate();
 
     if(AVI_write_frame(avifile2, out, out_len, key)<0) {
       AVI_print_error("avi video write error");
-      
-      return(TC_EXPORT_ERROR); 
-    }
+
+      return(TC_EXPORT_ERROR);
     
     return(0);
-    
+
   }
-  
-  if(param->flag == TC_AUDIO) return(audio_encode(param->buffer, param->size, avifile2));
-  
+
+  if(param->flag == TC_AUDIO)
+      return(audio_encode(param->buffer, param->size, avifile2));
+
   // invalid flag
   return(TC_EXPORT_ERROR);
 }
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * stop encoder
  *
  * ------------------------------------------------------------*/
 
-MOD_stop 
+MOD_stop
 {
-  
+
   if(param->flag == TC_VIDEO) {
 
     lzo_free(wrkmem);
     lzo_free(out);
-    
+
     return(0);
   }
-  
-  if(param->flag == TC_AUDIO) return(audio_stop());
-  
+
+  if(param->flag == TC_AUDIO)
+      return(audio_stop());
+
   return(TC_EXPORT_ERROR);
 }
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * close outputfiles
  *
  * ------------------------------------------------------------*/
 
 MOD_close
-{  
+{
 
   vob_t *vob = tc_get_vob();
 
@@ -298,8 +276,9 @@ MOD_close
     avifile1=NULL;
   }
 
-  if(param->flag == TC_AUDIO) return(audio_close());
-  
+  if(param->flag == TC_AUDIO)
+      return(audio_close());
+
   //outputfile
   if(vob->avifile_out!=NULL) {
     AVI_close(vob->avifile_out);
@@ -307,8 +286,8 @@ MOD_close
   }
 
   if(param->flag == TC_VIDEO) return(0);
-  
-  return(TC_EXPORT_ERROR);  
+
+  return(TC_EXPORT_ERROR);
 
 }
 
--- import/import_lzo.c
+++ import/import_lzo.c
@@ -1,31 +1,33 @@
 /*
  *  import_lzo.c
  *
- *  Copyright (C) Thomas Östreich - October 2002
+ *  Copyright (C) Thomas Oestreich - October 2002
  *
  *  This file is part of transcode, a video stream processing tool
- *      
+ *
  *  transcode is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2, or (at your option)
  *  any later version.
- *   
+ *
  *  transcode is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *   
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with GNU Make; see the file COPYING.  If not, write to
- *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  */
 
 #define MOD_NAME    "import_lzo.so"
-#define MOD_VERSION "v0.0.3 (2002-11-26)"
+#define MOD_VERSION "v0.1.0 (2005-10-16)"
 #define MOD_CODEC   "(video) LZO"
 
 #include "transcode.h"
+#include "magic.h"
+#include "libtc/tc_lzo.h"
 
 static int verbose_flag = TC_QUIET;
 static int capability_flag = TC_CAP_PCM | TC_CAP_YUV | TC_CAP_RGB |
@@ -34,15 +36,11 @@ static int capability_flag = TC_CAP_PCM | TC_CAP_YUV | TC_CAP_RGB |
 #define MOD_PRE lzo
 #include "import_def.h"
 
-#include <lzo1x.h>
-#if (LZO_VERSION > 0x1070)
-#  include <lzoutil.h>
-#endif
-
 
 static avi_t *avifile1=NULL;
 static avi_t *avifile2=NULL;
 
+static uint32_t video_codec;
 static int audio_codec;
 static int aframe_count=0, vframe_count=0;
 
@@ -55,7 +53,7 @@ static lzo_uint out_len;
 
 static int done_seek=0;
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * open stream
  *
@@ -80,12 +78,12 @@ MOD_open
 	if(NULL == (avifile2 = AVI_open_input_indexfile(vob->video_in_file,
                                                       0,vob->nav_seek_file))){
 	  AVI_print_error("avi open error");
-	  return(TC_IMPORT_ERROR); 
-	} 
+	  return(TC_IMPORT_ERROR);
+	}
       } else {
 	if(NULL == (avifile2 = AVI_open_input_file(vob->video_in_file,1))){
 	  AVI_print_error("avi open error");
-	  return(TC_IMPORT_ERROR); 
+	  return(TC_IMPORT_ERROR);
 	}
       }
     }
@@ -99,12 +97,20 @@ MOD_open
     //read all video parameter from input file
     width  =  AVI_video_width(avifile2);
     height =  AVI_video_height(avifile2);
-    
+
     fps    =  AVI_frame_rate(avifile2);
     codec  =  AVI_video_compressor(avifile2);
 
+    if (strcmp(codec,"LZO1") == 0) {
+      video_codec = TC_CODEC_LZO1;
+    } else if (strcmp(codec,"LZO2") == 0) {
+      video_codec = TC_CODEC_LZO2;
+    } else {
+      tc_log_warn(MOD_NAME, "Unsupported video codec %s", codec);
+      return(TC_IMPORT_ERROR);
+    }
 
-    fprintf(stderr, "[%s] codec=%s, fps=%6.3f, width=%d, height=%d\n", 
+    fprintf(stderr, "[%s] codec=%s, fps=%6.3f, width=%d, height=%d\n",
 	    MOD_NAME, codec, fps, width, height);
 
 
@@ -113,7 +119,7 @@ MOD_open
      */
 
     if (lzo_init() != LZO_E_OK) {
-      printf("[%s] lzo_init() failed\n", MOD_NAME);
+      fprintf(stderr, "[%s] lzo_init() failed\n", MOD_NAME);
       return(TC_IMPORT_ERROR); 
     }
 
@@ -121,7 +127,7 @@ MOD_open
     out = (lzo_bytep) lzo_malloc(BUFFER_SIZE);
 
     if (wrkmem == NULL || out == NULL) {
-      printf("[%s] out of memory\n", MOD_NAME);
+      fprintf(stderr, "[%s] out of memory\n", MOD_NAME);
       return(TC_IMPORT_ERROR); 
     }
 
@@ -132,17 +138,17 @@ MOD_open
 }
 
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * decode  stream
  *
  * ------------------------------------------------------------*/
 
-MOD_decode 
+MOD_decode
 {
 
   int key;
-
+  lzo_uint size;
   long bytes_read=0;
 
   if(param->flag == TC_VIDEO) {
@@ -153,25 +159,44 @@ MOD_decode
 
     out_len = AVI_read_frame(avifile2, out, &key);
 
-    if(verbose & TC_STATS && key) printf("keyframe %d\n", vframe_count); 
+    if(verbose & TC_STATS && key) 
+		fprintf(stderr, "keyframe %d\n", vframe_count);
 
     if(out_len<=0) {
       if(verbose & TC_DEBUG) AVI_print_error("AVI read video frame");
       return(TC_IMPORT_ERROR);
     }
 
-    r = lzo1x_decompress(out, out_len, param->buffer, &param->size, wrkmem);
+  	if (video_codec == TC_CODEC_LZO1) {
+    	r = lzo1x_decompress(out, out_len, param->buffer, &size, wrkmem);
+	} else {
+      tc_lzo_header_t *h = (tc_lzo_header_t *)out;
+      uint8_t *compdata = out + sizeof(*h);
+      int compsize = out_len - sizeof(*h);
+      if (h->magic != video_codec) {
+          tc_log_warn(MOD_NAME, "frame with invalid magic 0x%08X", h->magic);
+	      return (TC_IMPORT_ERROR);
+      }
+      if (h->flags & TC_LZO_NOT_COMPRESSIBLE) {
+          ac_memcpy(param->buffer, compdata, compsize);
+	      size = compsize;
+	      r = LZO_E_OK;
+      } else {
+	      r = lzo1x_decompress(compdata, compsize, param->buffer, &size, wrkmem);
+      }
+	}
 
     if (r == LZO_E_OK) {
-      if(verbose & TC_DEBUG) printf("decompressed %lu bytes into %lu bytes\n",
-				    (long) out_len, (long) param->size);
+      if(verbose & TC_DEBUG) 
+          fprintf(stderr, "decompressed %lu bytes into %lu bytes\n",
+				  (long) out_len, (long) param->size);
     } else {
-
       /* this should NEVER happen */
-      printf("[%s] internal error - decompression failed: %d\n", MOD_NAME, r);
+      fprintf(stderr, "[%s] internal error - decompression failed: %d\n", MOD_NAME, r);
       return(TC_IMPORT_ERROR); 
     }
 
+    param->size = size;
     //transcode v.0.5.0-pre8 addition
     if(key) param->attributes |= TC_FRAME_IS_KEYFRAME;
 
@@ -221,14 +246,14 @@ MOD_decode
   return(TC_IMPORT_ERROR);
 }
 
-/* ------------------------------------------------------------ 
+/* ------------------------------------------------------------
  *
  * close stream
  *
  * ------------------------------------------------------------*/
 
 MOD_close
-{  
+{
 
   if(param->fd != NULL) pclose(param->fd);
 
