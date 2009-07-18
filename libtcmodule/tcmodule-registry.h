cda6a8848a2f1ea7cb20a3b87bb56147f16f4add
Francesco Romani <fromani@gmail.com>
2009-07-18 10:00:12 +0200

    add the module registry.
    With the introduction of the module registry, the caller/client can now
    let the library automatically load the right module for a given codec/format,
    without the need of specifiying the exact module.
    Fallback list for a given format are allowed.
    
    The binding between formats and modules is specified by a configuration file.

---
+++ libtcmodule/tcmodule-registry.h
@@ -0,0 +1,70 @@
+/*
+ * tcmodule-registry.h -- module information registry.
+ * (C) 2009 - Francesco Romani <fromani -at- gmail -dot- com>
+ *
+ * This file is part of transcode, a video stream processing tool.
+ *
+ * transcode is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * transcode is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#ifndef TCMODULEREGISTRY_H
+#define TCMODULEREGISTRY_H
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "tcmodule-core.h"
+#include "tcmodule-data.h"
+
+#include <stdint.h>
+
+/*************************************************************************/
+
+/* registry data type. */
+typedef struct tcregistry_ *TCRegistry;
+
+
+TCRegistry tc_new_module_registry(TCFactory factory,
+                                  const char *regpath, int verbose);
+
+int tc_del_module_registry(TCRegistry registry);
+
+TCModule tc_new_module_for_format(TCRegistry registry,
+                                  const char *modclass,
+                                  const char *format,
+                                  int media);
+
+TCModule tc_new_module_most_fit(TCRegistry registry,
+                                const char *modclass,
+                                const char *fmtname, const char *modname,
+                                int media);
+
+const char *tc_module_registry_default_path(void);
+
+
+/*************************************************************************/
+
+#endif  /* TCMODULEREGISTRY_H */
+
+/*
+ * Local variables:
+ *   c-file-style: "stroustrup"
+ *   c-file-offsets: ((case-label . *) (statement-case-intro . *))
+ *   indent-tabs-mode: nil
+ * End:
+ *
+ * vim: expandtab shiftwidth=4:
+ */
